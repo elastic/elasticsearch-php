c4b61771225cccf341a74701d51003453aedabd5
Zachary Tong <zacharyjtong@gmail.com>
2013-05-03 16:08:29 -0400

    Flesh out ConnectionPool and tests

--- src/ElasticSearch/ConnectionPool/ConnectionPool.php
+++ src/ElasticSearch/ConnectionPool/ConnectionPool.php
@@ -9,44 +9,75 @@ namespace ElasticSearch\ConnectionPool;
 
 
 use ElasticSearch\Common\Exceptions;
+use ElasticSearch\Common\Exceptions\InvalidArgumentException;
 use ElasticSearch\ConnectionPool\Selectors\SelectorInterface;
+use ElasticSearch\ConnectionPool\DeadPool;
+use ElasticSearch\Connections\ConnectionInterface;
 
+/**
+ * Class ConnectionPool
+ *
+ * @category ElasticSearch
+ * @package  ElasticSearch\ConnectionPool\ConnectionPool
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
 class ConnectionPool
 {
 
     /**
+     * Array of connections
+     *
      * @var array
      */
-    protected $connections;
+    private $connections;
 
     /**
+     * Selector object, used to select a connection on each request
+     *
      * @var SelectorInterface
      */
-    protected $selector;
+    private $selector;
 
     /**
-     * @var int
-     */
-    protected $deadTimeout;
-
-    /**
+     * If set to true, connection list is randomized on construction
+     * Prevents Thundering Herds. Defaults to true
+     *
      * @var bool
      */
-    protected $randomizeHosts;
+    private $randomizeHosts;
 
     /**
-     * @var array
+     * Deadpool object contains a list of dead connections
+     * Responsible for retry timing and logic
+     *
+     * @var DeadPool
      */
-    protected $dead = array();
+    private $deadPool;
 
 
-    public function __construct($connections, $selector,  $deadTimeout=60, $randomizeHosts=true)
+    /**
+     * ConnectionPool Constructor
+     *
+     * @param array             $connections    Array of connections
+     * @param SelectorInterface $selector       Selector object used to return connections
+     * @param DeadPool          $deadPool       Deadpool object holds dead connections
+     * @param bool              $randomizeHosts If set to true, connections are randomized to prevent thundering herds
+     *
+     * @throws InvalidArgumentException
+     */
+    public function __construct($connections, $selector, $deadPool, $randomizeHosts=true)
     {
-        $paramList = array('connections', 'deadTimeout', 'selector', 'randomizeHosts');
+        $paramList = array('connections', 'deadPool', 'selector', 'randomizeHosts');
         foreach ($paramList as $param) {
             if (isset($$param) === false) {
-                throw new InvalidArgumentException('`'.$param.'` parameter must be set');
+                throw new InvalidArgumentException('`'.$param.'` parameter must not be null');
             }
+        }
+
+        if ($selector instanceof SelectorInterface !== true) {
+            throw new InvalidArgumentException('Selector parameter must implement SelectorInterface');
         }
 
         if ($randomizeHosts === true) {
@@ -55,22 +86,87 @@ class ConnectionPool
 
         $this->connections    = $connections;
         $this->selector       = $selector;
-        $this->deadTimeout    = $deadTimeout;
+        $this->deadPool       = $deadPool;
         $this->randomizeHosts = $randomizeHosts;
 
-    }
-
-    public function addConnection($connection)
-    {
-
-    }
+    }//end __construct()
 
 
     /**
+     * Find and return a connection from the pool
+     *
+     * @return ConnectionInterface
+     */
+    public function getConnection()
+    {
+        $this->resurrect();
+
+        // Still no connections in the pool?  Force a resurrection.
+        if (count($this->connections) === 0) {
+            $this->resurrect(true);
+        }
+
+        return $this->selector->select($this->connections);
+
+    }//end getConnection()
+
+
+    /**
+     * Query our deadpool to see if any nodes can be returned to the connection list
+     *
+     * @param bool $force If set to true, forces the Deadpool to resurrect a
+     * connection, even if the timeout has not expired yet
+     *
+     * @return void
+     */
+    public function resurrect($force=false)
+    {
+        array_push($this->connections, $this->deadPool->resurrect($force));
+
+    }//end resurrect()
+
+
+    /**
+     * Add a new connection to the pool
+     *
+     * @param ConnectionInterface $connection Connection object to add to the pool
+     *
+     * @return void
+     */
+    public function addConnection($connection)
+    {
+        $this->connections[] = $connection;
+
+    }//end addConnection()
+
+
+    /**
+     * Mark a connection as dead
+     * Removes the connection from the active list and adds
+     * it to the deadpool
+     *
+     * @param ConnectionInterface $connection Connection object to remove
+     *
+     * @return void
+     */
+    public function markDead($connection)
+    {
+        $position = array_search($connection, $this->connections, true);
+        $this->deadPool->markDead($this->connections[$position]);
+        unset($this->connections[$position]);
+
+    }//end markDead()
+
+
+    /**
+     * Return an array of all active connections
+     *
      * @return array
      */
-    public function getConnections()
+    public function getAllConnections()
     {
         return $this->connections;
-    }
+
+    }//end getAllConnections()
+
 }
--- tests/ElasticSearch/Tests/ConnectionPool/ConnectionPool.php
+++ tests/ElasticSearch/Tests/ConnectionPool/ConnectionPool.php
@@ -2,6 +2,7 @@
 
 namespace ElasticSearch\Tests\ConnectionPool;
 use ElasticSearch;
+use ElasticSearch\Connections\Connection;
 
 /**
  * Class ConnectionPool
@@ -37,5 +38,72 @@ class ConnectionPool extends \PHPUnit_Framework_TestCase
     }//end testNullConstructor()
 
 
+    /**
+     * Test adding one host and then requesting it
+     *
+     * @covers ConnectionPool::__construct
+     * @return void
+     */
+    public function testAddOneHostThenGetConnection()
+    {
+        $connections = array('host' => 'localhost', 'port' => 9200);
+        $deadPool    = $this->getMock('\ElasticSearch\ConnectionPool\DeadPool', array('resurrect'));
+        $deadPool->expects($this->once())
+            ->method('resurrect')
+            ->with($this->equalTo(false))
+            ->will($this->returnValue(array()));
+
+        $mockConnection = $this->getMockBuilder('\ElasticSearch\Connections\Connection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $selector = $this->getMock('\ElasticSearch\ConnectionPool\Selectors\RoundRobinSelector', array('select'));
+        $selector->expects($this->once())
+            ->method('select')
+            ->will($this->returnValue($mockConnection));
+
+        $randomizeHosts = true;
+        $connectionPool = new \ElasticSearch\ConnectionPool\ConnectionPool($connections, $selector, $deadPool, $randomizeHosts);
+
+        $retConnection = $connectionPool->getConnection();
+
+        $this->assertEquals($mockConnection, $retConnection);
+
+    }//end testAddOneHostThenGetConnection()
+
+
+    /**
+     * Get connection from deadpool by resurrection
+     *
+     * @covers ConnectionPool::__construct
+     * @return void
+     */
+    public function testResurrectFromDeadpool()
+    {
+        $connections = array();
+
+        $mockConnection = $this->getMockBuilder('\ElasticSearch\Connections\Connection')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $deadPool = $this->getMock('\ElasticSearch\ConnectionPool\DeadPool', array('resurrect'));
+        $deadPool->expects($this->once())
+            ->method('resurrect')
+            ->with($this->equalTo(false))
+            ->will($this->returnValue($mockConnection));
+
+        $selector = $this->getMock('\ElasticSearch\ConnectionPool\Selectors\RoundRobinSelector', array('select'));
+        $selector->expects($this->once())
+            ->method('select')
+            ->will($this->returnValue($mockConnection));
+
+        $randomizeHosts = true;
+        $connectionPool = new \ElasticSearch\ConnectionPool\ConnectionPool($connections, $selector, $deadPool, $randomizeHosts);
+
+        $retConnection = $connectionPool->getConnection();
+
+        $this->assertEquals($mockConnection, $retConnection);
+
+    }//end testAddOneHostThenGetConnection()
 
 }//end class

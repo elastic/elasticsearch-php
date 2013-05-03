9a7f71967f4a803126c2492d9e62b81b2d164389
Zachary Tong <zacharyjtong@gmail.com>
2013-05-03 17:52:53 -0400

    Implement RandomSelector

--- src/ElasticSearch/ConnectionPool/Selectors/RandomSelector.php
+++ src/ElasticSearch/ConnectionPool/Selectors/RandomSelector.php
@@ -7,7 +7,42 @@
 
 namespace ElasticSearch\ConnectionPool\Selectors;
 
+use ElasticSearch\Connections\ConnectionInterface;
 
-class RandomSelector {
+/**
+ * Class RandomSelector
+ *
+ * @category ElasticSearch
+ * @package  ElasticSearch\Connections\ConnectionInterface
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+class RandomSelector implements SelectorInterface
+{
 
+
+    /**
+     * Constructor
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }//end __construct()
+
+
+    /**
+     * Select a random connection from the provided array
+     *
+     * @param array $connections Array of Connection objects
+     *
+     * @return ConnectionInterface
+     */
+    public function select($connections) {
+
+        return $connections[array_rand($connections)];
+
+    }//end select()
 }

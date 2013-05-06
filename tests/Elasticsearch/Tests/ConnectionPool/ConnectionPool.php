b7dd8d65b5193222b200fb05903203617cf1a436
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 17:19:01 -0400

    Add test for markDead

--- tests/Elasticsearch/Tests/ConnectionPool/ConnectionPool.php
+++ tests/Elasticsearch/Tests/ConnectionPool/ConnectionPool.php
@@ -26,7 +26,7 @@ class ConnectionPool extends \PHPUnit_Framework_TestCase
      */
     public function testAddOneHostThenGetConnection()
     {
-        $connections = array('host' => 'localhost', 'port' => 9200);
+        $connections = array(array('host' => 'localhost', 'port' => 9200));
         $deadPool    = $this->getMock('\Elasticsearch\ConnectionPool\DeadPool', array('resurrect'));
         $deadPool->expects($this->once())
             ->method('resurrect')
@@ -85,5 +85,45 @@ class ConnectionPool extends \PHPUnit_Framework_TestCase
         $this->assertEquals($mockConnection, $retConnection);
 
     }//end testResurrectFromDeadpool()
+
+
+    /**
+     * Test adding multiple hosts, mark dead and verify
+     *
+     * @covers ConnectionPool::__construct
+     * @return void
+     */
+    public function testAddHostsMarkDeadThenVerify()
+    {
+        $connections = array();
+
+        $deadPool    = $this->getMock('\Elasticsearch\ConnectionPool\DeadPool', array('resurrect'));
+        $selector = $this->getMock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector');
+        $randomizeHosts = true;
+        $connectionPool = new \Elasticsearch\ConnectionPool\ConnectionPool($connections, $selector, $deadPool, $randomizeHosts);
+
+        $mockConnections = array();
+        foreach (range(1,10) as $index) {
+            $mockConnections[$index] = $this->getMockBuilder('\Elasticsearch\Connections\Connection')
+                ->disableOriginalConstructor()
+                ->getMock();
+
+            $connectionPool->addConnection($mockConnections[$index]);
+        }
+
+        // Kill a specific connection.
+        $connectionPool->markDead($mockConnections[5]);
+        $allConnections = $connectionPool->getAllConnections();
+
+        foreach (range(1,10) as $index) {
+            if ($index === 5) {
+                $this->assertNotContains($mockConnections[$index], $allConnections);
+            } else {
+                $this->assertContains($mockConnections[$index], $allConnections);
+            }
+        }
+
+
+    }//end testAddHostsMarkDeadThenVerify()
 
 }//end class

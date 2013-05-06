78270a0653c642728bbd6b8a00c76fe7cc8f35d2
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 11:25:57 -0400

    HostListConstructor skeleton

--- src/Elasticsearch/Sniffers/HostListConstructor.php
+++ src/Elasticsearch/Sniffers/HostListConstructor.php
@@ -7,11 +7,51 @@
 
 namespace Elasticsearch\Sniffers;
 
+use Elasticsearch\Transport;
 
+/**
+ * Class HostListConstructor
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
 class HostListConstructor
 {
-    public function __construct($host, $port)
+
+    /**
+     * @var Transport
+     */
+    private $transport;
+
+
+    /**
+     * Constructor
+     *
+     * @param Transport $transport Transport class, so
+     *                             that the schema can be determined
+     *
+     * @return \Elasticsearch\Sniffers\HostListConstructor
+     */
+    public function __construct(Transport $transport)
+    {
+        $this->transport = $transport;
+
+    }//end __construct()
+
+
+    /**
+     * Returns an array of host:ports derived from sniffing the Cluster API
+     *
+     * @param array $nodeInfo Array of Node Info from the API
+     *
+     * @return array
+     */
+    public function getHosts($nodeInfo)
     {
 
-    }
-}
+    }//end getHosts()
+
+}//end class

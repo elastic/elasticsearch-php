9c226bb919f5ee08418a1740fb4266eecfb17073
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 16:42:05 -0400

    Add SerializerInterface

---
+++ src/Elasticsearch/Serializers/SerializerInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/6/13
+ * Time: 4:40 AM
+ */
+
+namespace Elasticsearch\Serializers;
+
+interface SerializerInterface
+{
+    public function __construct();
+    public function serialize($data);
+    public function deserialize($data);
+}

bcec9f662d68c9f3e7f4e58e96e9dd6e6021a9a2
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 23:21:33 -0400

    Connection skeleton

--- src/Elasticsearch/Connections/Connection.php
+++ src/Elasticsearch/Connections/Connection.php
@@ -7,24 +7,57 @@
 
 namespace Elasticsearch\Connections;
 
-
-class Connection implements ConnectionInterface
+/**
+ * Class Connection
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Connections
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+class Connection extends BaseConnection implements ConnectionInterface
 {
-    private $transportSchema = 'http';
 
+
+    /**
+     * Constructor
+     *
+     * @param string $host Host string
+     * @param int    $port Host port
+     */
     public function __construct($host, $port=9200)
     {
+        parent::__construct($host, $port);
 
-    }
+    }//end __construct()
 
+
+    /**
+     * Returns the transport schema
+     *
+     * @return string
+     */
     public function getTransportSchema()
     {
         return $this->transportSchema;
-    }
 
+    }//end getTransportSchema()
+
+
+    /**
+     * Perform an HTTP request on the cluster
+     *
+     * @param string       $method HTTP method to use for request
+     * @param string       $uri    HTTP URI to use for request
+     * @param null|string  $params Optional URI parameters
+     * @param null|string  $body   Optional request body
+     *
+     * @return array
+     */
     public function performRequest($method, $uri, $params=null, $body=null)
     {
 
-    }
+    }//end performRequest()
 
 }

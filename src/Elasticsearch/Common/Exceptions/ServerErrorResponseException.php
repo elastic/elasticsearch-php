168c3350fd588f85ddaa8576b38fd1da821c3dee
Zachary Tong <zacharyjtong@gmail.com>
2013-05-07 16:21:15 -0400

    Add ServerErrorResponseException

---
+++ src/Elasticsearch/Common/Exceptions/ServerErrorResponseException.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/7/13
+ * Time: 3:04 PM
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Common\Exceptions
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+namespace Elasticsearch\Common\Exceptions;
+
+/**
+ * ServerErrorResponseException
+ */
+class ServerErrorResponseException extends TransportException implements ElasticsearchException {}

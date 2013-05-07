31152c71aee465d06815a0f46092bc8ed13b7a22
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 23:21:52 -0400

    Docblocks

--- src/Elasticsearch/Connections/ConnectionInterface.php
+++ src/Elasticsearch/Connections/ConnectionInterface.php
@@ -7,6 +7,15 @@
 
 namespace Elasticsearch\Connections;
 
+/**
+ * Interface ConnectionInterface
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Connections
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
 interface ConnectionInterface
 {
     public function __construct($host, $port);

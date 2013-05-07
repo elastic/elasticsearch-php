35b30e42ef8c4d8335adffb60b64fecbeebd57a3
Zachary Tong <zacharyjtong@gmail.com>
2013-05-07 16:21:03 -0400

    Add RuntimeException

---
+++ src/Elasticsearch/Common/Exceptions/RuntimeException.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/7/13
+ * Time: 11:51 AM
+ */
+
+/**
+ * @category Elasticsearch
+ * @package  Elasticsearch\Common\Exceptions
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+namespace Elasticsearch\Common\Exceptions;
+
+/**
+ * RuntimeException
+ */
+class RuntimeException extends \RuntimeException implements ElasticsearchException {}

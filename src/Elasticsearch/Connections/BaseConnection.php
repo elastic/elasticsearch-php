05b922902c498810b6a9e4152987d3cfc6162854
Zachary Tong <zacharyjtong@gmail.com>
2013-05-06 23:25:48 -0400

    Add base abstract class for connections
    Holds logging methods that are common to all connections

---
+++ src/Elasticsearch/Connections/BaseConnection.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/6/13
+ * Time: 11:00 PM
+ */
+
+namespace Elasticsearch\Connections;
+
+/**
+ * Abstrat Class BaseConnection
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Connections
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+abstract class BaseConnection
+{
+    /**
+     * @var string
+     */
+    protected $transportSchema = 'http';
+
+    /**
+     * @var string
+     */
+    private $host;
+
+
+    /**
+     * Constructor
+     *
+     * @param string $host Host string
+     * @param string $port Host port
+     *
+     * @return \Elasticsearch\Connections\BaseConnection
+     */
+    public function __construct($host, $port)
+    {
+        $this->host = $this->transportSchema.'://'.$host.':'.$port;
+
+    }//end __construct()
+
+
+    /**
+     * Log a successful request
+     *
+     * @param string $method
+     * @param string $fullURI
+     * @param string $path
+     * @param string $body
+     * @param string $statusCode
+     * @param string $response
+     * @param string $duration
+     *
+     * @return void
+     */
+    public function logRequestSuccess($method, $fullURI, $path, $body, $statusCode, $response, $duration)
+    {
+
+    }//end logRequestSuccess()
+
+
+    /**
+     * Log a a failed request
+     *
+     * @param string      $method
+     * @param string      $fullURI
+     * @param string      $body
+     * @param string      $duration
+     * @param null|string $statusCode
+     * @param null|string $response
+     * @param null|string $exception
+     *
+     * @return void
+     */
+    public function logRequestFail($method, $fullURI, $body, $duration, $statusCode=null, $response=null, $exception=null)
+    {
+
+    }//end logRequestFail
+
+}

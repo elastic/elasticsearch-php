22a0d86631c718b0d91b047ea2fb0593c6b858df
Zachary Tong <zacharyjtong@gmail.com>
2013-05-10 17:12:42 -0400

    Integration test for doc delete

--- tests/Elasticsearch/Tests/ClientIntegrationTest.php
+++ tests/Elasticsearch/Tests/ClientIntegrationTest.php
@@ -92,6 +92,50 @@ class ClientIntegrationTest extends \PHPUnit_Framework_TestCase
 
 
     /**
+     * Index a document, with ID, then retrieve it with a get
+     * then delete it
+     *
+     * @covers \Elasticsearch\Client::index
+     * @covers \Elasticsearch\Client::get
+     *
+     * @return void
+     */
+    public function testIndexDocumentWithIDThenDeleteThenGet()
+    {
+        $client = new Elasticsearch\Client();
+        $doc    = array('testField' => 'abc');
+        $index  = 'test';
+        $type   = 'test';
+        $id     = time();
+
+        $ret    = $client->index($index, $type, $doc, $id);
+        $this->assertEquals(1, $ret['ok']);
+        $this->assertEquals(1, $ret['_version']);
+        $this->assertEquals($index, $ret['_index']);
+        $this->assertEquals($type, $ret['_type']);
+        $this->assertEquals($id, $ret['_id']);
+
+        $retDoc = $client->get('test', $id);
+        $this->assertEquals(1, $retDoc['exists']);
+        $this->assertEquals($doc, $retDoc['_source']);
+        $this->assertEquals($id, $retDoc['_id']);
+        $this->assertEquals($index, $retDoc['_index']);
+        $this->assertEquals($type, $retDoc['_type']);
+        $this->assertEquals($ret['_version'], $retDoc['_version']);
+
+        $retDelete = $client->delete($index, $type, $id);
+        $this->assertEquals(1, $retDelete['ok']);
+        $this->assertEquals(1, $retDelete['found']);
+        $this->assertEquals($index, $retDelete['_index']);
+
+        $retDoc = $client->get('test', $id);
+        $this->assertNotEquals(1, $retDoc['exists']);
+
+
+    }//end testIndexDocumentWithIDThenDeleteThenGet()
+
+
+    /**
      * Index a document, then retrieve it with a search
      *
      * @covers \Elasticsearch\Client::index

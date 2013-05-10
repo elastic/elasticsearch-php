da8d456fbd311f0896ac28831cd2b57ad98c6b00
Zachary Tong <zacharyjtong@gmail.com>
2013-05-10 15:15:27 -0400

    Add checkParamWhitelist to abstract class

--- src/Elasticsearch/Namespaces/AbstractNamespace.php
+++ src/Elasticsearch/Namespaces/AbstractNamespace.php
@@ -7,6 +7,9 @@
 
 namespace Elasticsearch\Namespaces;
 
+use Elasticsearch\Common\Exceptions\UnexpectedValueException;
+use Elasticsearch\Transport;
+
 /**
  * Class AbstractNamespace
  *
@@ -20,8 +23,39 @@ abstract class AbstractNamespace
 {
     protected $transport;
 
+
+    /**
+     * Abstract constructor
+     *
+     * @param Transport $transport Transport object
+     */
     public function __construct($transport)
     {
         $this->transport = $transport;
-    }
+
+    }//end __construct()
+
+
+    /**
+     * Check if param is in whitelist
+     *
+     * @param array $params    Assoc array of parameters
+     * @param array $whitelist Whitelist of keys
+     *
+     * @throws UnexpectedValueException
+     * @return void
+     */
+    protected function checkParamWhitelist($params, $whitelist)
+    {
+        if (isset($params) !== true) {
+            return; //no params, just return.
+        }
+
+        foreach ($params as $key => $value) {
+            if (array_key_exists($key, $whitelist) === false) {
+                throw new UnexpectedValueException($key.' is not a valid parameter');
+            }
+        }
+
+    }//end checkParamWhitelist()
 }

a73e01895c6d62a712af7ff0194dd8ec6ee96918
Zachary Tong <zacharyjtong@gmail.com>
2013-05-31 09:38:28 -0400

    Refactor DIC construction to its own class
    DIC construction was getting unwieldy, and had no place
    being in the main Client class.  Refactored into its own
    class.  May further abstract shared/nonshared into their
    own classes to keep things small

--- src/Elasticsearch/Client.php
+++ src/Elasticsearch/Client.php
@@ -7,6 +7,7 @@
 
 namespace Elasticsearch;
 
+use Elasticsearch\Common\DICBuilder;
 use Elasticsearch\Common\Exceptions;
 use Elasticsearch\Common\Exceptions\UnexpectedValueException;
 use Elasticsearch\Namespaces\ClusterNamespace;
@@ -54,30 +55,6 @@ class Client
      */
     protected $cluster;
 
-    /**
-     * @var array
-     */
-    protected $paramDefaults = array(
-                                'connectionClass'       => '\Elasticsearch\Connections\CurlMultiConnection',
-                                'connectionPoolClass'   => '\Elasticsearch\ConnectionPool\ConnectionPool',
-                                'selectorClass'         => '\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector',
-                                'deadPoolClass'         => '\Elasticsearch\ConnectionPool\DeadPool',
-                                'snifferClass'          => '\Elasticsearch\Sniffers\Sniffer',
-                                'serializerClass'       => '\Elasticsearch\Serializers\JSONSerializer',
-                                'sniffOnStart'          => false,
-                                'snifferTimeout'        => false,
-                                'sniffOnConnectionFail' => false,
-                                'randomizeHosts'        => true,
-                                'maxRetries'            => 3,
-                                'deadTimeout'           => 60,
-                                'connectionParams'      => array(),
-                                'logObject'             => null,
-                                'logPath'               => 'elasticsearch.log',
-                                'logLevel'              => Logger::WARNING,
-                                'traceObject'           => null,
-                                'tracePath'             => 'elasticsearch.log',
-                                'traceLevel'            => Logger::WARNING,
-                               );
 
     /**
      * Client constructor
@@ -432,126 +409,12 @@ class Client
      * @param array $hosts  Array of hosts
      * @param array $params Array of user settings
      *
-     * @throws Common\Exceptions\InvalidArgumentException
-     * @throws Common\Exceptions\UnexpectedValueException
      * @return void
      */
     private function setParams($hosts, $params)
     {
-        if (isset($params) === false || is_array($params) !== true) {
-            throw new Exceptions\InvalidArgumentException('Parameters must be an array');
-        }
-
-        // Verify that all provided parameters are 'white-listed'.
-        foreach ($params as $key => $value) {
-            if (array_key_exists($key, $this->paramDefaults) === false) {
-                throw new Exceptions\UnexpectedValueException($key.' is not a recognized parameter');
-            }
-        }
-
-        $this->params = new \Pimple();
-
-        $params = array_merge($this->paramDefaults, $params);
-
-        foreach ($params as $key => $value) {
-            $this->params[$key] = $value;
-        }
-
-
-
-        $this->params['connection'] = function ($dicParams) {
-            return function ($host, $port=null) use ($dicParams) {
-                return new $dicParams['connectionClass'](
-                    $host,
-                    $port,
-                    $dicParams['connectionParamsShared'],
-                    $dicParams['logObject'],
-                    $dicParams['traceObject']
-                );
-            };
-        };
-
-        $this->params['selector'] = function ($dicParams) {
-            return new $dicParams['selectorClass']();
-        };
-
-        $this->params['deadPool'] = function ($dicParams) {
-            return new $dicParams['deadPoolClass'](
-                $dicParams['deadTimeout'],
-                $dicParams['logObject']
-            );
-        };
-
-        $this->params['sniffer'] = function ($dicParams) {
-            return function ($transport) use ($dicParams) {
-                return new $dicParams['snifferClass']($transport);
-            };
-        };
-
-        $this->params['serializer'] = function ($dicParams) {
-            return new $dicParams['serializerClass']();
-        };
-
-        $this->params['connectionPool'] = function ($dicParams) {
-            return function ($connections) use ($dicParams) {
-                return new $dicParams['connectionPoolClass'](
-                    $connections,
-                    $dicParams['selector'],
-                    $dicParams['deadPool'],
-                    $dicParams['randomizeHosts']);
-            };
-        };
-
-        /*
-            ----------------
-            Shared Section
-            ----------------
-        */
-
-        $this->params['transport'] = $this->params->share(
-            function ($dicParams) use ($hosts) {
-                return new Transport($hosts, $dicParams, $dicParams['logObject']);
-            }
-        );
-
-        $this->params['clusterNamespace'] = $this->params->share(
-            function ($dicParams) {
-                return new ClusterNamespace($dicParams['transport']);
-            }
-        );
-
-        $this->params['indicesNamespace'] = $this->params->share(
-            function ($dicParams) {
-                return new IndicesNamespace($dicParams['transport']);
-            }
-        );
-
-        // This will inject a shared object into all connections.
-        // Allows users to inject shared resources, similar to the multi-handle
-        // shared above (but in their own code).
-        $this->params['connectionParamsShared'] = $this->params->share(
-            function ($dicParams) {
-
-                $connectionParams = $dicParams['connectionParams'];
-
-                // Multihandle connections need a "static", shared curl multihandle.
-                if ($dicParams['connectionClass'] === '\Elasticsearch\Connections\CurlMultiConnection') {
-                    $connectionParams = array_merge(
-                        $connectionParams,
-                        array('curlMultiHandle' => $dicParams['curlMultiHandle'])
-                    );
-                }
-
-                return $connectionParams;
-            }
-        );
-
-        // Only used by some connections - won't be instantiated until used.
-        $this->params['curlMultiHandle'] = $this->params->share(
-            function () {
-                return curl_multi_init();
-            }
-        );
+        $dicBuilder = new DICBuilder($hosts, $params);
+        $this->params = $dicBuilder->getDIC();
 
     }//end setParams()
 
---
+++ src/Elasticsearch/Common/DICBuilder.php
@@ -0,0 +1,296 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/31/13
+ * Time: 8:26 AM
+ */
+
+namespace Elasticsearch\Common;
+
+
+use Elasticsearch\Namespaces\ClusterNamespace;
+use Elasticsearch\Namespaces\IndicesNamespace;
+use Elasticsearch\Transport;
+use Monolog\Logger;
+use Pimple;
+
+class DICBuilder
+{
+    /**
+     * @var \Pimple
+     */
+    private $dic;
+
+    /**
+     * @var array
+     */
+    protected $paramDefaults = array(
+                                'connectionClass'       => '\Elasticsearch\Connections\CurlMultiConnection',
+                                'connectionPoolClass'   => '\Elasticsearch\ConnectionPool\ConnectionPool',
+                                'selectorClass'         => '\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector',
+                                'deadPoolClass'         => '\Elasticsearch\ConnectionPool\DeadPool',
+                                'snifferClass'          => '\Elasticsearch\Sniffers\Sniffer',
+                                'serializerClass'       => '\Elasticsearch\Serializers\JSONSerializer',
+                                'sniffOnStart'          => false,
+                                'snifferTimeout'        => false,
+                                'sniffOnConnectionFail' => false,
+                                'randomizeHosts'        => true,
+                                'maxRetries'            => 3,
+                                'deadTimeout'           => 60,
+                                'connectionParams'      => array(),
+                                'logObject'             => null,
+                                'logPath'               => 'elasticsearch.log',
+                                'logLevel'              => Logger::WARNING,
+                                'traceObject'           => null,
+                                'tracePath'             => 'elasticsearch.log',
+                                'traceLevel'            => Logger::WARNING,
+                               );
+
+
+    /**
+     * Constructor
+     *
+     * @param array $hosts  Array of hosts
+     * @param array $params Array of user settings
+     *
+     * @throws Exceptions\InvalidArgumentException
+     * @throws Exceptions\UnexpectedValueException
+     */
+    public function __construct($hosts, $params)
+    {
+
+        if (isset($params) === false || is_array($params) !== true) {
+            throw new Exceptions\InvalidArgumentException(
+                'Parameters must be an array'
+            );
+        }
+
+        $this->checkParamWhitelist($params);
+        $this->createDICObject($hosts, $params);
+
+    }//end __construct()
+
+
+    /**
+     * Get the Dependency injection container
+     * 
+     * @return Pimple
+     */
+    public function getDIC()
+    {
+        return $this->dic;
+
+    }//end getDIC()
+
+
+    /**
+     * Verify that all parameters are whitelisted
+     * 
+     * @param array $params Array of params
+     *
+     * @throws Exceptions\UnexpectedValueException
+     * @return void
+     */
+    private function checkParamWhitelist($params)
+    {
+        // Verify that all provided parameters are 'white-listed'.
+        foreach ($params as $key => $value) {
+            if (array_key_exists($key, $this->paramDefaults) === false) {
+                throw new Exceptions\UnexpectedValueException(
+                    $key.' is not a recognized parameter'
+                );
+            }
+        }
+
+    }//end checkParamWhitelist()
+
+
+    /**
+     * Create the Pimple DIC object, build the dep tree
+     *
+     * @param array $hosts  Array of hosts
+     * @param array $params Array of user params
+     *
+     * @return void
+     */
+    private function createDICObject($hosts, $params)
+    {
+        $this->dic = new Pimple();
+
+        $this->setDICParams($params);
+        $this->setNonSharedDICObjects();
+        $this->setSharedDICObjects($hosts);
+
+    }//end createDICObject()
+
+
+    /**
+     * Set the user params inside the DIC object
+     *
+     * @param array $params Array of user params
+     *
+     * @return void
+     */
+    private function setDICParams($params)
+    {
+        $params = array_merge($this->paramDefaults, $params);
+
+        foreach ($params as $key => $value) {
+            $this->dic[$key] = $value;
+        }
+    }//end setDICParams()
+
+
+    /**
+     * NonShared DIC objects return a new obj each time
+     * they are accessed
+     */
+    private function setNonSharedDICObjects()
+    {
+        $this->setConnectionObj();
+        $this->setSelectorObj();
+        $this->setDeadpoolObj();
+        $this->setSnifferObj();
+        $this->setSerializerObj();
+        $this->setConnectionPoolObj();
+    }
+
+
+    /**
+     * Shared DIC objects reuse the same obj each time
+     * they are accessed
+     *
+     * @param array $hosts Array of hosts
+     */
+    private function setSharedDICObjects($hosts)
+    {
+        $this->setTransportObj($hosts);
+        $this->setClusterNamespaceObj();
+        $this->setIndicesNamespaceObj();
+        $this->setSharedConnectionParamsObj();
+        $this->setCurlMultihandle();
+    }
+
+
+    private function setConnectionObj()
+    {
+        $this->dic['connection'] = function ($dicParams) {
+            return function ($host, $port=null) use ($dicParams) {
+                return new $dicParams['connectionClass'](
+                    $host,
+                    $port,
+                    $dicParams['connectionParamsShared'],
+                    $dicParams['logObject'],
+                    $dicParams['traceObject']
+                );
+            };
+        };
+    }
+
+    private function setSelectorObj()
+    {
+        $this->dic['selector'] = function ($dicParams) {
+            return new $dicParams['selectorClass']();
+        };
+    }
+
+    private function setDeadpoolObj()
+    {
+        $this->dic['deadPool'] = function ($dicParams) {
+            return new $dicParams['deadPoolClass'](
+                $dicParams['deadTimeout'],
+                $dicParams['logObject']
+            );
+        };
+    }
+
+    private function setSnifferObj()
+    {
+        $this->dic['sniffer'] = function ($dicParams) {
+            return function ($transport) use ($dicParams) {
+                return new $dicParams['snifferClass']($transport);
+            };
+        };
+    }
+
+    private function setSerializerObj()
+    {
+        $this->dic['serializer'] = function ($dicParams) {
+            return new $dicParams['serializerClass']();
+        };
+    }
+
+    private function setConnectionPoolObj()
+    {
+        $this->dic['connectionPool'] = function ($dicParams) {
+            return function ($connections) use ($dicParams) {
+                return new $dicParams['connectionPoolClass'](
+                    $connections,
+                    $dicParams['selector'],
+                    $dicParams['deadPool'],
+                    $dicParams['randomizeHosts']);
+            };
+        };
+    }
+
+    private function setTransportObj($hosts)
+    {
+        $this->dic['transport'] = $this->dic->share(
+            function ($dicParams) use ($hosts) {
+                return new Transport($hosts, $dicParams, $dicParams['logObject']);
+            }
+        );
+    }
+
+    private function setClusterNamespaceObj()
+    {
+        $this->dic['clusterNamespace'] = $this->dic->share(
+            function ($dicParams) {
+                return new ClusterNamespace($dicParams['transport']);
+            }
+        );
+    }
+
+    private function setIndicesNamespaceObj()
+    {
+        $this->dic['indicesNamespace'] = $this->dic->share(
+            function ($dicParams) {
+                return new IndicesNamespace($dicParams['transport']);
+            }
+        );
+    }
+
+    private function setSharedConnectionParamsObj()
+    {
+        // This will inject a shared object into all connections.
+        // Allows users to inject shared resources, similar to the multi-handle
+        // shared above (but in their own code).
+        $this->dic['connectionParamsShared'] = $this->dic->share(
+            function ($dicParams) {
+
+                $connectionParams = $dicParams['connectionParams'];
+
+                // Multihandle connections need a "static", shared curl multihandle.
+                if ($dicParams['connectionClass'] === '\Elasticsearch\Connections\CurlMultiConnection') {
+                    $connectionParams = array_merge(
+                        $connectionParams,
+                        array('curlMultiHandle' => $dicParams['curlMultiHandle'])
+                    );
+                }
+
+                return $connectionParams;
+            }
+        );
+    }
+
+    private function setCurlMultihandle()
+    {
+        // Only used by some connections - won't be instantiated until used.
+        $this->dic['curlMultiHandle'] = $this->dic->share(
+            function () {
+                return curl_multi_init();
+            }
+        );
+    }
+
+}//end class

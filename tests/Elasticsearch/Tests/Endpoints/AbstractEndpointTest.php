1ea248a40fdcf95c3a8f1c3cb17330266ea9d784
Zachary Tong <zacharyjtong@gmail.com>
2013-06-04 12:20:19 -0400

    Add test for AbstractEndpoint

---
+++ tests/Elasticsearch/Tests/Endpoints/AbstractEndpointTest.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * User: zach
+ * Date: 6/4/13
+ * Time: 11:36 AM
+ */
+
+namespace Elasticsearch\Tests\Endpoints;
+
+use Elasticsearch\Common\Exceptions\UnexpectedValueException;
+use Elasticsearch\Endpoints\AbstractEndpoint;
+use Mockery as m;
+
+/**
+ * Class AbstractEndpointTest
+ * @package Elasticsearch\Tests\Endpoints
+ * @author  Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link    http://elasticsearch.org
+ */
+class AbstractEndpointTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function tearDown() {
+        m::close();
+    }
+
+    public function testPerformRequest()
+    {
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')
+                         ->with(
+                                 'GET',
+                                 '/testIndex/',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $stub = $this->getMockForAbstractClass('\Elasticsearch\Endpoints\AbstractEndpoint', array($mockTransport));
+
+        $stub->expects($this->once())
+            ->method('getURI')
+            ->will($this->returnValue('/testIndex/'));
+
+        $stub->expects($this->once())
+            ->method('getMethod')
+            ->will($this->returnValue('GET'));
+
+        $stub->performRequest();
+
+    }
+
+    public function testSetOneParam()
+    {
+        $mockTransport = m::mock('\Elasticsearch\Transport');
+
+        $stub = $this->getMockForAbstractClass('\Elasticsearch\Endpoints\AbstractEndpoint', array($mockTransport));
+
+        $paramWhiteList = array(
+            'param1',
+            'param2',
+            'param3'
+        );
+
+        $stub->expects($this->once())
+            ->method('getParamWhitelist')
+            ->will($this->returnValue($paramWhiteList));
+
+
+        $params = array('param1');
+        $stub->setParams($params);
+
+    }
+
+    public function testSetMultipleParam()
+    {
+        $mockTransport = m::mock('\Elasticsearch\Transport');
+
+        $stub = $this->getMockForAbstractClass('\Elasticsearch\Endpoints\AbstractEndpoint', array($mockTransport));
+
+        $paramWhiteList = array(
+            'param1',
+            'param2',
+            'param3'
+        );
+
+        $stub->expects($this->once())
+        ->method('getParamWhitelist')
+        ->will($this->returnValue($paramWhiteList));
+
+        $stub->setParams($paramWhiteList);
+
+    }
+
+
+    /**
+     * @expectedException UnexpectedValueException
+     */
+    public function testSetInvalidParam()
+    {
+        $mockTransport = m::mock('\Elasticsearch\Transport');
+
+        $stub = $this->getMockForAbstractClass('\Elasticsearch\Endpoints\AbstractEndpoint', array($mockTransport));
+
+        $paramWhiteList = array(
+            'param1',
+            'param2',
+            'param3'
+        );
+
+        $stub->expects($this->once())
+        ->method('getParamWhitelist')
+        ->will($this->returnValue($paramWhiteList));
+
+        $params = array('param10');
+        $stub->setParams($params);
+
+    }
+
+}

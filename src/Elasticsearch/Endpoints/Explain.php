2bb7a78b75478716ab63028ec5727cfba056b16d
Zachary Tong <zacharyjtong@gmail.com>
2013-06-06 08:18:52 -0400

    Cleanup autogenerated Explain Endpoint

--- src/Elasticsearch/Endpoints/Explain.php
+++ src/Elasticsearch/Endpoints/Explain.php
@@ -18,98 +18,19 @@ class Explain extends AbstractEndpoint
 {
 
     /**
-     *TODO Validate auto-generated file
-     *     Implement per-class specific functions if required
-
-{
-  "explain": {
-    "documentation": "http://elasticsearch.org/guide/reference/api/explain/",
-    "methods": ["GET", "POST"],
-    "url": {
-      "path": "/{index}/{type}/{id}/_explain",
-      "paths": ["/{index}/{type}/{id}/_explain"],
-      "parts": {
-        "id": {
-          "type" : "string",
-          "required" : true,
-          "description" : "The document ID"
-        },
-        "index": {
-          "type" : "string",
-          "required" : true,
-          "description" : "The name of the index"
-        },
-        "type": {
-          "type" : "string",
-          "required" : true,
-          "description" : "The type of the document"
-        }
-      },
-      "params": {
-        "analyze_wildcard": {
-          "type" : "boolean",
-          "description" : "Specify whether wildcards and prefix queries in the query string query should be analyzed (default: false)"
-        },
-        "analyzer": {
-          "type" : "string",
-          "description" : "The analyzer for the query string query"
-        },
-        "default_operator": {
-          "type" : "enum",
-          "options" : ["AND","OR"],
-          "default" : "OR",
-          "description" : "The default operator for query string query (AND or OR)"
-        },
-        "df": {
-          "type" : "string",
-          "description" : "The default field for query string query (default: _all)"
-        },
-        "fields": {
-          "type": "list",
-          "description" : "A comma-separated list of fields to return in the response"
-        },
-        "lenient": {
-          "type" : "boolean",
-          "description" : "Specify whether format-based query failures (such as providing text to a numeric field) should be ignored"
-        },
-        "lowercase_expanded_terms": {
-          "type" : "boolean",
-          "description" : "Specify whether query terms should be lowercased"
-        },
-        "parent": {
-          "type" : "string",
-          "description" : "The ID of the parent document"
-        },
-        "preference": {
-          "type" : "string",
-          "description" : "Specify the shards the operation should be performed on (default: random shard)"
-        },
-        "q": {
-          "type" : "string",
-          "description" : "Query in the Lucene query string syntax"
-        },
-        "routing": {
-          "type" : "string",
-          "description" : "Specific routing value"
-        },
-        "source": {
-          "type" : "string",
-          "description" : "The URL-encoded query definition (instead of using the request body)"
-        }
-      }
-    },
-    "body": {
-      "required" : true,
-      "description" : "The query definition using the Query DSL"
-    }
-  }
-}
-
-
+     * @param array $body
+     *
+     * @return $this
      */
+    public function setBody($body)
+    {
+        $this->body = $body;
+        return $this;
+    }
 
 
     /**
+     * @throws \Elasticsearch\Common\Exceptions\RuntimeException
      * @return string
      */
     protected function getURI()
@@ -133,9 +54,9 @@ class Explain extends AbstractEndpoint
             );
         }
 
-        $id = $this->id;
+        $id    = $this->id;
         $index = $this->index;
-        $type = $this->type;
+        $type  = $this->type;
         $uri   = "/$index/$type/$id/_explain";
 
         return $uri;
@@ -167,7 +88,6 @@ class Explain extends AbstractEndpoint
      */
     protected function getMethod()
     {
-        //TODO Fix Me!
-        return 'GET,POST';
+        return 'GET';
     }
 }

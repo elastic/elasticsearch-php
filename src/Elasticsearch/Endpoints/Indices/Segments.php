ccde60c20703546ff5b42985dff776a309dc3941
Zachary Tong <zacharyjtong@gmail.com>
2013-06-07 14:54:35 -0400

    Cleanup autogenerated Indices\Segments Endpoint

--- src/Elasticsearch/Endpoints/Indices/Segments.php
+++ src/Elasticsearch/Endpoints/Indices/Segments.php
@@ -18,57 +18,16 @@ class Segments extends AbstractEndpoint
 {
 
     /**
-     *TODO Validate auto-generated file
-     *     Implement per-class specific functions if required
-
-{
-  "indices.segments": {
-    "documentation": "http://elasticsearch.org/guide/reference/api/admin-indices-segments/",
-    "methods": ["GET"],
-    "url": {
-      "path": "/_segments",
-      "paths": ["/_segments", "/{index}/_segments"],
-      "parts": {
-        "index": {
-          "type" : "list",
-          "required" : true,
-          "description" : "A comma-separated list of index names; use `_all` or empty string to perform the operation on all indices"
-        }
-      },
-      "params": {
-        "ignore_indices": {
-          "type" : "enum",
-          "options" : ["none","missing"],
-          "default" : "none",
-          "description" : "When performed on multiple indices, allows to ignore `missing` ones"
-        },
-        "operation_threading": {
-          "description" : "TODO: ?"
-        }
-      }
-    },
-    "body": null
-  }
-}
-
-
-     */
-
-
-    /**
      * @return string
      */
     protected function getURI()
     {
-
-        if (isset($this->index) !== true) {
-            throw new Exceptions\RuntimeException(
-                'index is required for Segments'
-            );
-        }
-
         $index = $this->index;
         $uri   = "/_segments";
+
+        if (isset($index) === true) {
+            $uri = "/$index/_segments";
+        }
 
         return $uri;
     }

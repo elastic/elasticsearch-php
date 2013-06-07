3272df559f47968b392adc04348ebb80dfa173a7
Zachary Tong <zacharyjtong@gmail.com>
2013-06-07 14:24:56 -0400

    Cleanup autogenerated Indices\Flush Endpoint

--- src/Elasticsearch/Endpoints/Indices/Flush.php
+++ src/Elasticsearch/Endpoints/Indices/Flush.php
@@ -18,66 +18,17 @@ class Flush extends AbstractEndpoint
 {
 
     /**
-     *TODO Validate auto-generated file
-     *     Implement per-class specific functions if required
-
-{
-  "indices.flush": {
-    "documentation": "http://www.elasticsearch.org/guide/reference/api/admin-indices-flush/",
-    "methods": ["POST", "GET"],
-    "url": {
-      "path": "/_flush",
-      "paths": ["/_flush", "/{index}/_flush"],
-      "parts": {
-        "index": {
-          "type" : "list",
-          "required" : true,
-          "description" : "A comma-separated list of index names; use `_all` or empty string for all indices"
-        }
-      },
-      "params": {
-        "force": {
-          "type" : "boolean",
-          "description" : "TODO: ?"
-        },
-        "full": {
-          "type" : "boolean",
-          "description" : "TODO: ?"
-        },
-        "ignore_indices": {
-          "type" : "enum",
-          "options" : ["none","missing"],
-          "default" : "none",
-          "description" : "When performed on multiple indices, allows to ignore `missing` ones"
-        },
-        "refresh": {
-          "type" : "boolean",
-          "description" : "Refresh the index after performing the operation"
-        }
-      }
-    },
-    "body": null
-  }
-}
-
-
-     */
-
-
-    /**
      * @return string
      */
     protected function getURI()
     {
 
-        if (isset($this->index) !== true) {
-            throw new Exceptions\RuntimeException(
-                'index is required for Flush'
-            );
-        }
-
         $index = $this->index;
         $uri   = "/_flush";
+
+        if (isset($index) === true) {
+            $uri = "/$index/_flush";
+        }
 
         return $uri;
     }
@@ -100,7 +51,6 @@ class Flush extends AbstractEndpoint
      */
     protected function getMethod()
     {
-        //TODO Fix Me!
-        return 'POST,GET';
+        return 'POST';
     }
 }

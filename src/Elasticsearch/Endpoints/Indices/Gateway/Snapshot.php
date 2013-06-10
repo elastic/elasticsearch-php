ec62061f5415372bc7f1931abcb6ee2350e46c78
Zachary Tong <zacharyjtong@gmail.com>
2013-06-10 12:47:38 -0400

    Cleanup autogenerated Indices\Gateway\Snapshot Endpoint

--- src/Elasticsearch/Endpoints/Indices/Gateway/Snapshot.php
+++ src/Elasticsearch/Endpoints/Indices/Gateway/Snapshot.php
@@ -18,53 +18,17 @@ class Snapshot extends AbstractEndpoint
 {
 
     /**
-     *TODO Validate auto-generated file
-     *     Implement per-class specific functions if required
-
-{
-  "indices.gateway.snapshot": {
-    "documentation": "http://www.elasticsearch.org/guide/reference/api/admin-indices-gateway-snapshot/",
-    "methods": ["POST"],
-    "url": {
-      "path": "/_gateway/snapshot",
-      "paths": ["/_gateway/snapshot", "/{index}/_gateway/snapshot"],
-      "parts": {
-        "index": {
-          "type" : "list",
-          "description" : "A comma-separated list of index names; use `_all` or empty string for all indices"
-        }
-      },
-      "params": {
-        "ignore_indices": {
-          "type" : "enum",
-          "options" : ["none","missing"],
-          "default" : "none",
-          "description" : "When performed on multiple indices, allows to ignore `missing` ones"
-        }
-      }
-    },
-    "body": null
-  }
-}
-
-
-     */
-
-
-    /**
      * @return string
      */
     protected function getURI()
     {
-
-        if (isset($this->index) !== true) {
-            throw new Exceptions\RuntimeException(
-                'index is required for Snapshot'
-            );
-        }
-
         $index = $this->index;
         $uri   = "/_gateway/snapshot";
+
+
+        if (isset($index) === true) {
+            $uri = "/$index/_gateway/snapshot";
+        }
 
         return $uri;
     }

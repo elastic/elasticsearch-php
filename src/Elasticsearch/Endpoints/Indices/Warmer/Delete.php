9f6033d7f90daebf7ed534d55c96f00fe12f9fb8
Zachary Tong <zacharyjtong@gmail.com>
2013-06-10 16:04:30 -0400

    Cleanup autogenerated Indices\Warmer\Delete Endpoint

--- src/Elasticsearch/Endpoints/Indices/Warmer/Delete.php
+++ src/Elasticsearch/Endpoints/Indices/Warmer/Delete.php
@@ -14,45 +14,8 @@ use Elasticsearch\Common\Exceptions;
  * Class Delete
  * @package Elasticsearch\Endpoints\Indices\Warmer
  */
-class Delete extends AbstractEndpoint
+class Delete extends AbstractWarmerEndpoint
 {
-
-    /**
-     *TODO Validate auto-generated file
-     *     Implement per-class specific functions if required
-
-{
-  "indices.warmer.delete": {
-    "documentation": "http://www.elasticsearch.org/guide/reference/api/admin-indices-warmers/",
-    "methods": ["DELETE"],
-    "url": {
-      "path": "/{index}/_warmer",
-      "paths": ["/{index}/_warmer", "/{index}/_warmer/{name}", "/{index}/{type}/_warmer/{name}"],
-      "parts": {
-        "index": {
-          "type" : "list",
-          "required": true,
-          "description" : "A comma-separated list of index names to register warmer for; use `_all` or empty string to perform the operation on all indices"
-        },
-        "name" : {
-          "type" : "string",
-          "description" : "The name of the warmer (supports wildcards); leave empty to delete all warmers"
-        },
-        "type": {
-          "type" : "list",
-          "description" : "A comma-separated list of document types to register warmer for; use `_all` or empty string to perform the operation on all types"
-        }
-      },
-      "params": {
-      }
-    },
-    "body": null
-  }
-}
-
-
-     */
-
 
     /**
      * @return string
@@ -60,18 +23,11 @@ class Delete extends AbstractEndpoint
     protected function getURI()
     {
 
-        $index = $this->index;
-        $name = $this->name;
-        $type = $this->type;
-        $uri   = "/$index/_warmer";
+        $uri = $this->getOptionalURI('_warmer');
 
-        if (isset($index) === true) {
-            $uri = "/$index/_warmer/$name";
-        } elseif (isset($name) === true && isset($index) === true) {
-            $uri = "/$index/$type/_warmer/$name";
-        }
- elseif (isset($type) === true && isset($name) === true && isset($index) === true) {
-            $uri = "";
+        $name = $this->name;
+        if (isset($name) === true) {
+            $uri .= "/$name";
         }
 
         return $uri;
---
+++ tests/Elasticsearch/Tests/Endpoints/Indices/Warmer/DeleteTest.php
@@ -0,0 +1,119 @@
+<?php
+/**
+ * User: zach
+ * Date: 6/10/13
+ * Time: 4:01 PM
+ */
+
+namespace Elasticsearch\Tests\Endpoints\Indices\Warmer;
+
+use Elasticsearch\Common\Exceptions\RuntimeException;
+use Elasticsearch\Endpoints\Indices\Warmer\Delete;
+use Mockery as m;
+
+/**
+ * Class DeleteTest
+ * @package Elasticsearch\Tests\Endpoints\Indices\Warmer
+ * @author  Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link    http://elasticsearch.org
+ */
+class DeleteTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown() {
+        m::close();
+    }
+
+
+    public function testDeleteNoNameNoIndexNoType()
+    {
+
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')->once()
+                         ->with(
+                                 'DELETE',
+                                 '/_all/_warmer',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $action = new Delete($mockTransport);
+        $action->performRequest();
+
+    }
+
+    public function testDeleteWithIndex()
+    {
+
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')->once()
+                         ->with(
+                                 'DELETE',
+                                 '/testIndex/_warmer',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $action = new Delete($mockTransport);
+        $action->setIndex('testIndex')->performRequest();
+
+    }
+
+    public function testDeleteWithType()
+    {
+
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')->once()
+                         ->with(
+                                 'DELETE',
+                                 '/_all/testType/_warmer',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $action = new Delete($mockTransport);
+        $action->setType('testType')->performRequest();
+
+    }
+
+    public function testDeleteWithName()
+    {
+
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')->once()
+                         ->with(
+                                 'DELETE',
+                                 '/_all/_warmer/testName',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $action = new Delete($mockTransport);
+        $action->setName('testName')->performRequest();
+
+    }
+
+    public function testDeleteWithIndexTypeAndName()
+    {
+
+        $mockTransport = m::mock('\Elasticsearch\Transport')
+                         ->shouldReceive('performRequest')->once()
+                         ->with(
+                                 'DELETE',
+                                 '/testIndex/testType/_warmer/testName',
+                                 array(),
+                                 null
+                             )
+                         ->getMock();
+
+        $action = new Delete($mockTransport);
+        $action->setIndex('testIndex')->setType('testType')->setName('testName')->performRequest();
+
+    }
+
+
+}

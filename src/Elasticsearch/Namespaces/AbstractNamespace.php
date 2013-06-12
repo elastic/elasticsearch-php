56c86db8d746634cf7dbd39ebe0ce75ae728575f
Zachary Tong <zacharyjtong@gmail.com>
2013-06-12 08:43:25 -0400

    Remove unused method

--- src/Elasticsearch/Namespaces/AbstractNamespace.php
+++ src/Elasticsearch/Namespaces/AbstractNamespace.php
@@ -36,26 +36,4 @@ abstract class AbstractNamespace
     }
 
 
-    /**
-     * Check if param is in whitelist
-     *
-     * @param array $params    Assoc array of parameters
-     * @param array $whitelist Whitelist of keys
-     *
-     * @throws UnexpectedValueException
-     * @return void
-     */
-    protected function checkParamWhitelist($params, $whitelist)
-    {
-        if (isset($params) !== true) {
-            return; //no params, just return.
-        }
-
-        foreach ($params as $key => $value) {
-            if (array_search($key, $whitelist) === false) {
-                throw new UnexpectedValueException($key . ' is not a valid parameter');
-            }
-        }
-
-    }
 }

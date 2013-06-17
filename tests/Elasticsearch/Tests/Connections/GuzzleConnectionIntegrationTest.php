8adfa9f29dd4aa4e87d84660b9eaa628c86ede0b
Zachary Tong <zacharyjtong@gmail.com>
2013-06-17 15:40:47 -0400

    Basic integration test

---
+++ tests/Elasticsearch/Tests/Connections/GuzzleConnectionIntegrationTest.php
@@ -0,0 +1,161 @@
+<?php
+/**
+ * User: zach
+ * Date: 6/17/13
+ * Time: 2:43 PM
+ */
+
+namespace Elasticsearch\Tests\Connections;
+use Elasticsearch;
+use Elasticsearch\Common\Exceptions\Curl;
+use Guzzle\Http\Client;
+use Mockery as m;
+use Elasticsearch\Connections\GuzzleConnection;
+
+
+/**
+ * Class GuzzleConnectionTest
+ *
+ * @category   Tests
+ * @package    Elasticsearch
+ * @subpackage Tests/Connections
+ * @author     Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link       http://elasticsearch.org
+ */
+class GuzzleConnectionIntegrationTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+
+    /**
+     * @expectedException Elasticsearch\Common\Exceptions\Curl\CouldNotResolveHostException
+     */
+    public function test5xxErrorBadHost()
+    {
+        $host = 'localhost5';
+        $port = 9200;
+
+        $connectionParams['guzzleClient'] = new Client();
+
+        $log = m::mock('\Monolog\Logger')->shouldReceive('addError')->once()->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+        $ret = $connection->performRequest('GET', '/');
+
+    }
+
+
+    /**
+     * @expectedException Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost
+     */
+    public function test5xxErrorBadPort()
+    {
+        $host = 'localhost';
+        $port = 9800;
+        $connectionParams['guzzleClient'] = new Client();
+        $log = m::mock('\Monolog\Logger')->shouldReceive('addError')->once()->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+        $ret = $connection->performRequest('GET', '/');
+
+    }
+
+    public function test4xxErrorNonexistantEndpoint()
+    {
+        $host = 'localhost';
+        $port = 9200;
+        $connectionParams['guzzleClient'] = new Client();
+        $log = m::mock('\Monolog\Logger')
+                ->shouldReceive('addDebug')
+                ->times(6)->getMock()
+                ->shouldReceive('addInfo')
+                ->times(4)->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+        $ret = $connection->performRequest('GET', '/abc');
+
+        $this->assertEquals(400, $ret['status']);
+
+    }
+
+    public function testQueryParams()
+    {
+        $host = 'localhost';
+        $port = 9200;
+        $connectionParams['guzzleClient'] = new Client();
+        $log = m::mock('\Monolog\Logger')
+               ->shouldReceive('addDebug')
+               ->times(3)->getMock()
+               ->shouldReceive('addInfo')
+               ->times(2)->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+        $params['pretty'] = 'true';
+
+        $ret = $connection->performRequest('GET', '/', $params);
+
+        $this->assertEquals(200, $ret['status']);
+
+        $expectedURI = 'http://localhost:9200/?pretty=true';
+        $this->assertEquals($expectedURI, $ret['info']['url']);
+
+    }
+
+    public function testQueryURI()
+    {
+        $host = 'localhost';
+        $port = 9200;
+        $connectionParams['guzzleClient'] = new Client();
+        $log = m::mock('\Monolog\Logger')
+               ->shouldReceive('addDebug')
+               ->times(3)->getMock()
+               ->shouldReceive('addInfo')
+               ->times(2)->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+
+        $ret = $connection->performRequest('GET', '/_cluster/nodes/');
+
+        $expectedURI = 'http://localhost:9200/_cluster/nodes/';
+        $this->assertEquals($expectedURI, $ret['info']['url']);
+
+    }
+
+
+    public function test4xxErrorInvalidIndexAndQueryBody()
+    {
+        $host = 'localhost';
+        $port = 9200;
+        $connectionParams['guzzleClient'] = new Client();
+        $log = m::mock('\Monolog\Logger')
+               ->shouldReceive('addDebug')
+               ->times(6)->getMock()
+               ->shouldReceive('addInfo')
+               ->times(4)->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+
+        $body = '{"testsetting":"123"}';
+
+        /*
+            The index _doesnotexist is used with an underscore
+            because ES won't create it...invalid.
+        */
+
+        $ret = $connection->performRequest('POST', '/_doesnotexist', null, $body);
+
+        $this->assertEquals(400, $ret['status']);
+
+        $expectedURI = 'http://localhost:9200/_doesnotexist';
+        $this->assertEquals($expectedURI, $ret['info']['url']);
+
+        // Best we can do to make sure the post actually posted.
+        $this->assertEquals(strlen(($body)), $ret['info']['size_upload']);
+
+
+    }
+}
---
+++ tests/Elasticsearch/Tests/Connections/GuzzleConnectionTest.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/7/13
+ * Time: 2:19 PM
+ */
+
+namespace Elasticsearch\Tests\Connections;
+use Elasticsearch;
+use Mockery as m;
+use Elasticsearch\Connections\GuzzleConnection;
+
+
+/**
+ * Class GuzzleConnectionTest
+ *
+ * @category   Tests
+ * @package    Elasticsearch
+ * @subpackage Tests/Connections
+ * @author     Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link       http://elasticsearch.org
+ */
+class GuzzleConnectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    /**
+     * @expectedException \Elasticsearch\Common\Exceptions\InvalidArgumentException
+     */
+    public function testNoGuzzleClient()
+    {
+        $host = 'localhost';
+        $port = 9200;
+        $connectionParams = null;
+
+        $log = m::mock('\Monolog\Logger')->shouldReceive('addCritical')->once()->getMock();
+
+        $connection = new GuzzleConnection($host, $port, $connectionParams, $log, $log);
+
+    }
+
+}

9f2674ac291c6c607a0ed50348c1cd8fa7cc0816
Zachary Tong <zacharyjtong@gmail.com>
2013-07-22 13:53:34 -0400

    Cleanup autogenerated DeleteByQuery endpoint

---
+++ src/Elasticsearch/Endpoints/DeleteByQuery.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * User: zach
+ * Date: 05/31/2013
+ * Time: 16:47:11 pm
+ */
+
+namespace Elasticsearch\Endpoints;
+
+use Elasticsearch\Endpoints\AbstractEndpoint;
+use Elasticsearch\Common\Exceptions;
+
+/**
+ * Class DeleteByQuery
+ * @package Elasticsearch\Endpoints
+ */
+class DeleteByQuery extends AbstractEndpoint
+{
+
+    /**
+     * @param array $body
+     *
+     * @throws \Elasticsearch\Common\Exceptions\InvalidArgumentException
+     * @return $this
+     */
+    public function setBody($body)
+    {
+        if (isset($body) !== true) {
+            return $this;
+        }
+
+        if (is_array($body) !== true) {
+            throw new Exceptions\InvalidArgumentException(
+                'Body must be an array'
+            );
+        }
+        $this->body = $body;
+        return $this;
+    }
+
+    /**
+     * @throws \Elasticsearch\Common\Exceptions\RuntimeException
+     * @return string
+     */
+    protected function getURI()
+    {
+        if (isset($this->index) !== true) {
+            throw new Exceptions\RuntimeException(
+                'index is required for Delete'
+            );
+        }
+
+        $index = $this->index;
+        $type  = $this->type;
+        $uri   = "/$index/_query";
+
+        if (isset($type) === true) {
+            $uri = "/$index/$type/_query";
+        }
+
+        return $uri;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getParamWhitelist()
+    {
+        return array(
+            'consistency',
+            'ignore_indices',
+            'replication',
+            'routing',
+            'source',
+            'timeout',
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getMethod()
+    {
+        return 'DELETE';
+    }
+}

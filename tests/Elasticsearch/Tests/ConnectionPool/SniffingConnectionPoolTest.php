f6c7bbfa52a18f2fdec8ddff96534db8fd9aa908
Zachary Tong <zacharyjtong@gmail.com>
2013-09-20 13:59:26 -0400

    More tests

--- tests/Elasticsearch/Tests/ConnectionPool/SniffingConnectionPoolTest.php
+++ tests/Elasticsearch/Tests/ConnectionPool/SniffingConnectionPoolTest.php
@@ -40,13 +40,77 @@ class SniffingConnectionPoolTest extends \PHPUnit_Framework_TestCase
 
         $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory');
 
-        $randomizeHosts = false;
-        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $randomizeHosts);
+        $connectionPoolParams = array('randomizeHosts' => false);
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
 
         $retConnection = $connectionPool->nextConnection();
 
         $this->assertEquals($mockConnection, $retConnection);
 
+    }
+
+    public function testAddOneHostAndTriggerSniff()
+    {
+        $clusterState = json_decode('{"ok":true,"cluster_name":"elasticsearch_zach","nodes":{"Bl2ihSr7TcuUHxhu1GA_YQ":{"name":"Vesta","transport_address":"inet[/192.168.1.119:9300]","hostname":"zach-ThinkPad-W530","version":"0.90.5","http_address":"inet[/192.168.1.119:9200]"}}}', true);
+
+        $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                          ->shouldReceive('ping')->andReturn(true)->getMock()
+                          ->shouldReceive('isAlive')->andReturn(true)->getMock()
+                          ->shouldReceive('getTransportSchema')->once()->andReturn('http')->getMock()
+                          ->shouldReceive('sniff')->once()->andReturn($clusterState)->getMock();
+
+        $connections = array($mockConnection);
+        $mockNewConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                             ->shouldReceive('isAlive')->andReturn(true)->getMock();
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')->once()
+                    ->andReturn($mockNewConnection)
+                    ->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory')
+                    ->shouldReceive('create')->with('192.168.1.119', 9200)->andReturn($mockNewConnection)->getMock();
+
+        $connectionPoolParams = array(
+            'randomizeHosts' => false,
+            'sniffingInterval'  => -1
+        );
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection();
+
+        $this->assertEquals($mockNewConnection, $retConnection);
+    }
+
+    public function testAddOneHostAndForceNext()
+    {
+        $clusterState = json_decode('{"ok":true,"cluster_name":"elasticsearch_zach","nodes":{"Bl2ihSr7TcuUHxhu1GA_YQ":{"name":"Vesta","transport_address":"inet[/192.168.1.119:9300]","hostname":"zach-ThinkPad-W530","version":"0.90.5","http_address":"inet[/192.168.1.119:9200]"}}}', true);
+
+        $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                          ->shouldReceive('ping')->andReturn(true)->getMock()
+                          ->shouldReceive('isAlive')->andReturn(true)->getMock()
+                          ->shouldReceive('getTransportSchema')->once()->andReturn('http')->getMock()
+                          ->shouldReceive('sniff')->once()->andReturn($clusterState)->getMock();
+
+        $connections = array($mockConnection);
+        $mockNewConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                             ->shouldReceive('isAlive')->andReturn(true)->getMock();
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')->once()->andReturn($mockConnection)->getMock()
+                    ->shouldReceive('select')->once()->andReturn($mockNewConnection)->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory')
+                             ->shouldReceive('create')->with('192.168.1.119', 9200)->andReturn($mockNewConnection)->getMock();
+
+        $connectionPoolParams = array(
+            'randomizeHosts' => false
+        );
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection(true);
+
+        $this->assertEquals($mockNewConnection, $retConnection);
     }
 
 }

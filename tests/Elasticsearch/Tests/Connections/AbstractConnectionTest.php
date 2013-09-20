71a56b1a4c52c0a573ec7284f32eaab54a1f52ef
Zachary Tong <zacharyjtong@gmail.com>
2013-09-20 18:50:09 -0400

    Test ping

---
+++ tests/Elasticsearch/Tests/Connections/AbstractConnectionTest.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * User: zach
+ * Date: 9/20/13
+ * Time: 6:38 PM
+ */
+
+namespace Elasticsearch\Tests\Connections;
+use Elasticsearch;
+use Elasticsearch\Connections\AbstractConnection;
+use Mockery as m;
+
+
+
+/**
+ * Class AbstractConnectionTest
+ *
+ * @category   Tests
+ * @package    AbstractConnectionTest
+ * @subpackage Tests/Connections
+ * @author     Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license    http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link       http://elasticsearch.org
+ */
+class AbstractConnectionTest extends \PHPUnit_Framework_TestCase
+{
+    public function tearDown()
+    {
+        m::close();
+    }
+
+    public function testPing()
+    {
+        $stub = $this->getMockForAbstractClass(
+            'Elasticsearch\Connections\AbstractConnection',
+            array(
+                'localhost',
+                9200,
+                array(),
+                null,
+                null
+            )
+        );
+
+        $options = array('timeout' => 1);
+        $return = array('status' => 200);
+
+        $stub->expects($this->once())
+             ->method('performRequest')
+             ->with('HEAD', '', null, null, $options)
+             ->will($this->returnValue($return));
+
+        /** @var AbstractConnection $stub */
+        $ret = $stub->ping();
+
+        $this->assertTrue($ret);
+    }
+
+    public function testPingBadCode()
+    {
+        $stub = $this->getMockForAbstractClass(
+            'Elasticsearch\Connections\AbstractConnection',
+            array(
+                'localhost',
+                9200,
+                array(),
+                null,
+                null
+            )
+        );
+
+        $options = array('timeout' => 1);
+        $return = array('status' => 400);
+
+        $stub->expects($this->once())
+             ->method('performRequest')
+             ->with('HEAD', '', null, null, $options)
+             ->will($this->returnValue($return));
+
+        /** @var AbstractConnection $stub */
+        $ret = $stub->ping();
+
+        $this->assertFalse($ret);
+    }
+
+
+    public function testPingTimeout()
+    {
+        $stub = $this->getMockForAbstractClass(
+            'Elasticsearch\Connections\AbstractConnection',
+            array(
+                'localhost',
+                9200,
+                array(),
+                null,
+                null
+            )
+        );
+
+        $options = array('timeout' => 1);
+        $return = array('status' => 400);
+
+        $stub->expects($this->once())
+             ->method('performRequest')
+             ->with('HEAD', '', null, null, $options)
+             ->will($this->throwException(new Elasticsearch\Common\Exceptions\Curl\OperationTimeoutException()));
+
+        /** @var AbstractConnection $stub */
+        $ret = $stub->ping();
+
+        $this->assertFalse($ret);
+    }
+
+}
+

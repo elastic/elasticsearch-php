85a07fe8389098ca198f5434dce19c8ee05d95cc
Zachary Tong <zacharyjtong@gmail.com>
2013-09-20 16:38:59 -0400

    More tests

--- tests/Elasticsearch/Tests/ConnectionPool/SniffingConnectionPoolTest.php
+++ tests/Elasticsearch/Tests/ConnectionPool/SniffingConnectionPoolTest.php
@@ -5,6 +5,7 @@
  * Time: 12:51 PM
  */
 
+use Elasticsearch\Common\Exceptions\NoNodesAvailableException;
 use Elasticsearch\ConnectionPool\SniffingConnectionPool;
 use Mockery as m;
 
@@ -113,4 +114,159 @@ class SniffingConnectionPoolTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals($mockNewConnection, $retConnection);
     }
 
+
+    public function testAddTenNodesThenGetConnection()
+    {
+        $connections = array();
+
+        foreach (range(1,10) as $index) {
+            $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                              ->shouldReceive('ping')
+                              ->andReturn(true)
+                              ->getMock()
+                              ->shouldReceive('isAlive')
+                              ->andReturn(true)
+                              ->getMock();
+
+            $connections[] = $mockConnection;
+        }
+
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')
+                    ->andReturn($connections[0])
+                    ->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory');
+
+        $connectionPoolParams = array('randomizeHosts' => false);
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection();
+
+        $this->assertEquals($connections[0], $retConnection);
+
+    }
+
+    public function testAddTenNodesTimeoutAllButLast()
+    {
+        $connections = array();
+
+        foreach (range(1,9) as $index) {
+            $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                              ->shouldReceive('ping')
+                              ->andReturn(false)
+                              ->getMock()
+                              ->shouldReceive('isAlive')
+                              ->andReturn(false)
+                              ->getMock();
+
+            $connections[] = $mockConnection;
+        }
+
+        $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                          ->shouldReceive('ping')
+                          ->andReturn(true)
+                          ->getMock()
+                          ->shouldReceive('isAlive')
+                          ->andReturn(true)
+                          ->getMock();
+
+        $connections[] = $mockConnection;
+
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')
+                    ->andReturnValues($connections)
+                    ->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory');
+
+        $connectionPoolParams = array('randomizeHosts' => false);
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection();
+
+        $this->assertEquals($connections[9], $retConnection);
+
+    }
+
+
+    /**
+     * @expectedException Elasticsearch\Common\Exceptions\NoNodesAvailableException
+     */
+    public function testAddTenNodesAllTimeout()
+    {
+        $connections = array();
+
+        foreach (range(1,10) as $index) {
+            $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                              ->shouldReceive('ping')
+                              ->andReturn(false)
+                              ->getMock()
+                              ->shouldReceive('isAlive')
+                              ->andReturn(false)
+                              ->getMock();
+
+            $connections[] = $mockConnection;
+        }
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')
+                    ->andReturnValues($connections)
+                    ->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory');
+
+        $connectionPoolParams = array('randomizeHosts' => false);
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection();
+
+    }
+
+    public function testAddOneHostSniffTwo()
+    {
+        $clusterState = json_decode('{"ok":true,"cluster_name":"elasticsearch_zach","nodes":{"node1":{"name":"Vesta","transport_address":"inet[/192.168.1.119:9300]","hostname":"zach-ThinkPad-W530","version":"0.90.5","http_address":"inet[/192.168.1.119:9200]"}, "node2":{"name":"Vesta","transport_address":"inet[/192.168.1.119:9301]","hostname":"zach-ThinkPad-W530","version":"0.90.5","http_address":"inet[/192.168.1.119:9201]"}}}', true);
+
+        $mockConnection = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                          ->shouldReceive('ping')->andReturn(true)->getMock()
+                          ->shouldReceive('isAlive')->andReturn(true)->getMock()
+                          ->shouldReceive('getTransportSchema')->once()->andReturn('http')->getMock()
+                          ->shouldReceive('sniff')->once()->andReturn($clusterState)->getMock();
+
+        $connections = array($mockConnection);
+
+        $newConnections = array();
+        $newConnections[] = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                             ->shouldReceive('isAlive')->andReturn(true)->getMock();
+
+        $newConnections[] = m::mock('\Elasticsearch\Connections\GuzzleConnection')
+                             ->shouldReceive('isAlive')->andReturn(true)->getMock();
+
+        $selector = m::mock('\Elasticsearch\ConnectionPool\Selectors\RoundRobinSelector')
+                    ->shouldReceive('select')
+                    ->andReturnValues(array(        //selects provided node first, then the new cluster list
+                            $mockConnection,
+                            $newConnections[0],
+                            $newConnections[1]
+                    ))
+                    ->getMock();
+
+        $connectionFactory = m::mock('\Elasticsearch\Connections\ConnectionFactory')
+                             ->shouldReceive('create')->with('192.168.1.119', 9200)->andReturn($newConnections[0])->getMock()
+                             ->shouldReceive('create')->with('192.168.1.119', 9201)->andReturn($newConnections[1])->getMock();
+
+        $connectionPoolParams = array(
+            'randomizeHosts' => false,
+            'sniffingInterval'  => -1
+        );
+        $connectionPool = new SniffingConnectionPool($connections, $selector, $connectionFactory, $connectionPoolParams);
+
+        $retConnection = $connectionPool->nextConnection();
+        $this->assertEquals($newConnections[0], $retConnection);
+
+        $retConnection = $connectionPool->nextConnection();
+        $this->assertEquals($newConnections[1], $retConnection);
+    }
 }

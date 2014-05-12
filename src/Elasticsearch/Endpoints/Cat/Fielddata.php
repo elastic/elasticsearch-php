950fbaad0e6a0ae0301b91638a833b0c0c985d82
Zachary Tong <zacharyjtong@gmail.com>
2014-05-12 10:51:29 -0400

    Add Cat/Fielddata endpoint

---
+++ src/Elasticsearch/Endpoints/Cat/Fielddata.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * User: zach
+ * Date: 5/12/14
+ * Time: 10:40 AM
+ */
+
+namespace Elasticsearch\Endpoints\Cat;
+
+use Elasticsearch\Endpoints\AbstractEndpoint;
+use Elasticsearch\Common\Exceptions;
+
+/**
+ * Class Fielddata
+ *
+ * @category Elasticsearch
+ * @package Elasticsearch\Endpoints\Cat
+ * @author   Zachary Tong <zachary.tong@elasticsearch.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elasticsearch.org
+ */
+
+class Fielddata extends AbstractEndpoint
+{
+    private $fields;
+
+
+    /**
+     * @param $fields
+     *
+     * @return $this
+     */
+    public function setFields($fields)
+    {
+        if (isset($fields) !== true) {
+            return $this;
+        }
+
+        $this->fields = $fields;
+        return $this;
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getURI()
+    {
+        $fields = $this->fields;
+        $uri   = "/_cat/fielddata";
+
+        if (isset($fields) === true) {
+            $uri = "/_cat/aliases/$fields";
+        }
+
+        return $uri;
+    }
+
+
+    /**
+     * @return string[]
+     */
+    protected function getParamWhitelist()
+    {
+        return array(
+            'local',
+            'master_timeout',
+            'h',
+            'help',
+            'v',
+        );
+    }
+
+
+    /**
+     * @return string
+     */
+    protected function getMethod()
+    {
+        return 'GET';
+    }
+}

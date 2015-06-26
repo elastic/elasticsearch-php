2b4769c62ba4d3446cdcb5a0f9d597383c30fe5d
Zachary Tong <zacharyjtong@gmail.com>
2015-06-26 14:45:03 -0400

    [TEST] fix bug where array_key_exists explodes due to doubles

--- tests/Elasticsearch/Tests/YamlRunnerTest.php
+++ tests/Elasticsearch/Tests/YamlRunnerTest.php
@@ -284,11 +284,15 @@ class YamlRunnerTest extends \PHPUnit_Framework_TestCase
 
         if (is_array($values) === true) {
             array_walk_recursive($values, function(&$item, $key) use ($stash) {
-                if (is_string($item) === true || is_numeric($item) === true) {
+                if (is_string($item) === true) {
                     if (array_key_exists($item, $stash) == true) {
                         $item = $stash[$item];
                     }
-                } elseif (is_object($item) === true) {
+                } elseif (is_numeric($item) === true) {
+                    if (array_key_exists((int)$item, $stash) == true) {
+                        $item = $stash[$item];
+                    }
+                } elseif  (is_object($item) === true) {
 
                     $tItem = json_decode(json_encode($item), true);
 

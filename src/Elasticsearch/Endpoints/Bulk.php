d434bf0070d6d8ec52c19eb422a9d100313a04a0
Zachary Tong <zacharyjtong@gmail.com>
2015-08-05 21:17:10 -0400

    Add 'fields' param to Bulk Endpoint

--- src/Elasticsearch/Endpoints/Bulk.php
+++ src/Elasticsearch/Endpoints/Bulk.php
@@ -68,6 +68,7 @@ class Bulk extends AbstractEndpoint implements BulkEndpointInterface
             'refresh',
             'replication',
             'type',
+            'fields'
         );
     }
 

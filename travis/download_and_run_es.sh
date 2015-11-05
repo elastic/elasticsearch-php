c92a5adfd1b4923ff77f5818ebac09ba4835b7bf
Zachary Tong <zacharyjtong@gmail.com>
2015-11-05 18:23:13 -0500

    travis: more script tweaks

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -4,20 +4,21 @@ if [ -z $ES_VERSION ]; then
     exit 1;
 fi;
 
-ES_DIR="elasticsearch-latest-SNAPSHOT"
 
 killall java 2>/dev/null
 
-if [ ! -d $ES_DIR ]; then
-    echo "Downloading Elasticsearch v${ES_VERSION}-SNAPSHOT"
-    ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/$ES_VERSION/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip"
-    curl -L -O $ES_URL
-    unzip "elasticsearch-latest-SNAPSHOT.zip"
-    echo 'repositories.url.allowed_urls: ["http://*"]' >> ./*/config/elasticsearch.yml
-fi;
+
+echo "Downloading Elasticsearch v${ES_VERSION}-SNAPSHOT..."
+ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/$ES_VERSION/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip"
+curl -L -O $ES_URL
+unzip "elasticsearch-latest-SNAPSHOT.zip"
+
+echo "Adding repo to config..."
+echo 'repositories.url.allowed_urls: ["http://*"]' >> ./elasticsearch-*/config/elasticsearch.yml
+
 
 echo "Starting Elasticsearch v${ES_VERSION}"
-./*/bin/elasticsearch \
+./elasticsearch-*/bin/elasticsearch \
     -Des.network.host=localhost \
     -Des.discovery.zen.ping.multicast.enabled=false \
     -Des.discovery.zen.ping_timeout=1s \

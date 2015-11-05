31eb9e0af2531e6623b79aafaa3b0a394b31930d
Zachary Tong <zacharyjtong@gmail.com>
2015-11-05 16:24:25 -0500

    [Tests] Delete unreliable, bad tests
    Unreliable integration tests posing as unit tests.  Needs to be rewritten with proper mocking

--- tests/Elasticsearch/Tests/ClientTest.php
+++ tests/Elasticsearch/Tests/ClientTest.php
@@ -23,53 +23,12 @@ class ClientTest extends \PHPUnit_Framework_TestCase
         m::close();
     }
 
-    public function testOneGoodOneBadHostNoException()
-    {
-        $client = Elasticsearch\ClientBuilder::create()->setHosts(['127.0.0.1:80', $_SERVER['ES_TEST_HOST']])->build();
-
-        // Perform three requests to make sure the bad host is tried at least once
-        $client->info();
-        $client->info();
-        $client->info();
-    }
-
-    /**
-     * @expectedException Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost
-     */
-    public function testOneGoodOneBadHostNoRetryException()
-    {
-        $client = Elasticsearch\ClientBuilder::create()->setHosts(['127.0.0.1:80', $_SERVER['ES_TEST_HOST']])->setRetries(0)->build();
-
-        // Perform three requests to make sure the bad host is tried at least once
-        $client->exists(array("index" => 'test', 'type' => 'test', 'id' => 'test'));
-        $client->exists(array("index" => 'test', 'type' => 'test', 'id' => 'test'));
-        $client->exists(array("index" => 'test', 'type' => 'test', 'id' => 'test'));
-    }
-
-    /**
-     * @expectedException Elasticsearch\Common\Exceptions\NoNodesAvailableException
-     */
-    public function testBadHost()
-    {
-        $client = Elasticsearch\ClientBuilder::create()->setHosts(['127.0.0.1:80'])->build();
-        $response = $client->info();
-    }
-
     /**
      * @expectedException \Elasticsearch\Common\Exceptions\InvalidArgumentException
      */
     public function testConstructorIllegalPort()
     {
         $client = Elasticsearch\ClientBuilder::create()->setHosts(['localhost:abc'])->build();
-    }
-
-    /**
-     * @expectedException \Elasticsearch\Common\Exceptions\Curl\CouldNotConnectToHost
-     */
-    public function testZeroRetries()
-    {
-        $client = Elasticsearch\ClientBuilder::create()->setHosts(['127.0.0.1:80'])->setRetries(0)->build();
-        $client->exists(array("index" => 'test', 'type' => 'test', 'id' => 'test'));
     }
 
     public function testCustomQueryParams()

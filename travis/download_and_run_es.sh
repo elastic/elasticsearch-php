fe96602ae499c6d27990cc9d12b0bd19a19b7d26
Zachary Tong <zacharyjtong@gmail.com>
2015-11-05 16:57:24 -0500

    travis: hardcode unzipping dir

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -4,15 +4,15 @@ if [ -z $ES_VERSION ]; then
     exit 1;
 fi;
 
-ES_DIR="elasticsearch-$ES_VERSION"
+ES_DIR="elasticsearch-latest-SNAPSHOT"
 
 killall java 2>/dev/null
 
 if [ ! -d $ES_DIR ]; then
-    echo "Downloading Elasticsearch v${ES_VERSION}"
+    echo "Downloading Elasticsearch v${ES_VERSION}-SNAPSHOT"
     ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/$ES_VERSION/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip"
     curl -L -O $ES_URL
-    unzip "elasticsearch-latest-SNAPSHOT.zip"
+    unzip "elasticsearch-latest-SNAPSHOT.zip" -d "${ES_DIR}"
 fi;
 
 echo "Starting Elasticsearch v${ES_VERSION}"

16dfe8a2298fb953028b80dfc26d9908f8118bf1
Zachary Tong <zacharyjtong@gmail.com>
2016-03-30 16:40:57 -0400

    [TEST] Automate snapshot retrieval

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -10,19 +10,9 @@ killall java 2>/dev/null
 
 echo "Downloading Elasticsearch v${ES_VERSION}-SNAPSHOT..."
 
+ES_URL=$(curl -sS "https://esvm-props.kibana.rocks/builds" | jq -r ".branches.$ES_VERSION.zip")
 
-# Sourced from https://github.com/elastic/ci/blob/master/client_tests_urls.prop
-# Note: currently URLs are all the same format, but sometimes it changes
-# TODO automate this
-case $ES_VERSION in
-    '2.2') ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/2.2/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip" ;;
-	'2.1') ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/2.1/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip" ;;
-  '2.0') ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/2.0/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip" ;;
-  '1.7') ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/1.7/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip" ;;
-  '1.6') ES_URL="http://s3-eu-west-1.amazonaws.com/build.eu-west-1.elastic.co/origin/1.6/nightly/JDK7/elasticsearch-latest-SNAPSHOT.zip" ;;
-esac
-
-curl -L -O $ES_URL
+curl -L -o elasticsearch-latest-SNAPSHOT.zip $ES_URL
 unzip "elasticsearch-latest-SNAPSHOT.zip"
 
 echo "Adding repo to config..."

8341c4c3f0a3ea09b961432e271c97f164a98325
Zachary Tong <zacharyjtong@gmail.com>
2016-05-31 12:14:16 -0400

    Tweak SpecParser template

--- util/SpecParser.php
+++ util/SpecParser.php
@@ -179,7 +179,13 @@ function generateTemplate($path, $template)
 
     $data['url']['processed'] = processURLPaths($data);
     $data['url']['default'] = getDefaultPath($data['url']['processed'][count($data['url']['processed'])-1]);
-
+    if (count($data['methods']) > 1) {
+        if (in_array("GET", $data['methods'])) {
+            $data['methods'] = "GET";
+        } else {
+            $data['methods'] = $data['methods'][0];
+        }
+    }
     $renderVars = array(
         'json'      => $json,
         'data'      => $data,

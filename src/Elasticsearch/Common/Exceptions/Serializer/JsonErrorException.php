0a7fd55067dc7f25f15706804eec7c12793b1549
Zachary Tong <zacharyjtong@gmail.com>
2016-11-15 13:05:04 -0500

    Add new (undocumented) PHP-7 JSON error codes, better unknown handling
    Closes #499

--- src/Elasticsearch/Common/Exceptions/Serializer/JsonErrorException.php
+++ src/Elasticsearch/Common/Exceptions/Serializer/JsonErrorException.php
@@ -31,18 +31,19 @@ class JsonErrorException extends \Exception implements ElasticsearchException
         JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
         JSON_ERROR_SYNTAX => 'Syntax error',
         JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded',
+        JSON_ERROR_RECURSION => 'One or more recursive references in the value to be encoded',
+        JSON_ERROR_INF_OR_NAN => 'One or more NAN or INF values in the value to be encoded',
+        JSON_ERROR_UNSUPPORTED_TYPE => 'A value of a type that cannot be encoded was given',
 
-        // JSON_ERROR_* constant values that are available on PHP >= 5.5.0
-        6 => 'One or more recursive references in the value to be encoded',
-        7 => 'One or more NAN or INF values in the value to be encoded',
-        8 => 'A value of a type that cannot be encoded was given',
-
+        // JSON_ERROR_* constant values that are available on PHP >= 7.0
+        9 => 'Decoding of value would result in invalid PHP property name', //JSON_ERROR_INVALID_PROPERTY_NAME
+        10 => 'Attempted to decode nonexistent UTF-16 code-point' //JSON_ERROR_UTF16
     );
 
     public function __construct($code, $input, $result, $previous = null)
     {
         if (isset(self::$messages[$code]) !== true) {
-            throw new \InvalidArgumentException(sprintf('%d is not a valid JSON error code.', $code));
+            throw new \InvalidArgumentException(sprintf('Encountered unknown JSON error code: [%d]', $code));
         }
 
         parent::__construct(self::$messages[$code], $code, $previous);

b262dca5828e9952e14981395dfe508d22f80dbf
Christopher Davis <cdavis9999@gmail.com>
2016-12-16 16:10:16 -0500

    Re-Add the DeleteByQuery Functionality (#513)
    * Re-Add the DeleteByQuery Functionality
    
    Reverts 9f3776a224ef4c65e75df7d004ee37361f64567f and brings the
    `DeleteByQuery` endpoint up to spec with the current API.
    
    * Remove `slice` as a Parameter from DeleteByQuery
    
    It's in the request body, not the query string.
    
    * Include All the DeleteByQuery Params
    
    Generated from a bit of scripting around the delete by query api spec.

---
+++ src/Elasticsearch/Endpoints/DeleteByQuery.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace Elasticsearch\Endpoints;
+
+use Elasticsearch\Common\Exceptions;
+
+/**
+ * Class Deletebyquery
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Endpoints
+ * @author   Zachary Tong <zach@elastic.co>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elastic.co
+ */
+class DeleteByQuery extends AbstractEndpoint
+{
+    /**
+     * @param array $body
+     *
+     * @throws \Elasticsearch\Common\Exceptions\InvalidArgumentException
+     * @return $this
+     */
+    public function setBody($body)
+    {
+        if (isset($body) !== true) {
+            return $this;
+        }
+
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @throws \Elasticsearch\Common\Exceptions\RuntimeException
+     * @return string
+     */
+    protected function getURI()
+    {
+        if (!$this->index) {
+            throw new Exceptions\RuntimeException(
+                'index is required for Deletebyquery'
+            );
+        }
+
+        $uri = "/{$this->index}/_delete_by_query";
+        if ($this->type) {
+            $uri = "/{$this->index}/{$this->type}/_delete_by_query";
+        }
+
+        return $uri;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getParamWhitelist()
+    {
+        return array(
+            '_source',
+            '_source_exclude',
+            '_source_include',
+            'allow_no_indices',
+            'analyze_wildcard',
+            'analyzer',
+            'conflicts',
+            'default_operator',
+            'df',
+            'expand_wildcards',
+            'from',
+            'ignore_unavailable',
+            'lenient',
+            'preference',
+            'q',
+            'refresh',
+            'request_cache',
+            'requests_per_second',
+            'routing',
+            'scroll',
+            'scroll_size',
+            'search_timeout',
+            'search_type',
+            'size',
+            'slices',
+            'sort',
+            'stats',
+            'terminate_after',
+            'timeout',
+            'version',
+            'wait_for_active_shards',
+            'wait_for_completion',
+        );
+    }
+
+    /**
+     * @return string
+     */
+    protected function getMethod()
+    {
+        return 'POST';
+    }
+}

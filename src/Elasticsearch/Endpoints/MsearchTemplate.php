df7004f08800fc8be11053355e98b619358c37fd
Zachary Tong <zacharyjtong@gmail.com>
2017-07-19 14:42:25 -0400

    Add MsearchTemplate endpoint
    Closes #599

---
+++ src/Elasticsearch/Endpoints/MsearchTemplate.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Elasticsearch\Endpoints;
+
+use Elasticsearch\Common\Exceptions;
+use Elasticsearch\Serializers\SerializerInterface;
+use Elasticsearch\Transport;
+
+/**
+ * Class MsearchTemplate
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Endpoints
+ * @author   Zachary Tong <zach@elastic.co>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elastic.co
+ */
+class MsearchTemplate extends AbstractEndpoint
+{
+    /**
+     * @param SerializerInterface $serializer
+     */
+    public function __construct(SerializerInterface $serializer)
+    {
+        $this->serializer = $serializer;
+    }
+
+    /**
+     * @param array|string $body
+     *
+     * @throws \Elasticsearch\Common\Exceptions\InvalidArgumentException
+     * @return $this
+     */
+    public function setBody($body)
+    {
+        if (isset($body) !== true) {
+            return $this;
+        }
+
+        if (is_array($body) === true) {
+            $bulkBody = "";
+            foreach ($body as $item) {
+                $bulkBody .= $this->serializer->serialize($item)."\n";
+            }
+            $body = $bulkBody;
+        }
+
+        $this->body = $body;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getURI()
+    {
+        $index = $this->index;
+        $type = $this->type;
+        $uri   = "/_msearch/template";
+
+        if (isset($index) === true && isset($type) === true) {
+            $uri = "/$index/$type/_msearch/template";
+        } elseif (isset($index) === true) {
+            $uri = "/$index/_msearch/template";
+        } elseif (isset($type) === true) {
+            $uri = "/_all/$type/_msearch/template";
+        }
+
+        return $uri;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getParamWhitelist()
+    {
+        return array(
+            'search_type',
+            'typed_keys',
+            'max_concurrent_searches'
+        );
+    }
+
+    /**
+     * @return array
+     * @throws \Elasticsearch\Common\Exceptions\RuntimeException
+     */
+    public function getBody()
+    {
+        if (isset($this->body) !== true) {
+            throw new Exceptions\RuntimeException('Body is required for MSearch');
+        }
+
+        return $this->body;
+    }
+
+    /**
+     * @return string
+     */
+    public function getMethod()
+    {
+        return 'GET';
+    }
+}

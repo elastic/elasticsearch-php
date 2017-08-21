e0f0985a6f6e970bac7d7e574f32b2525b16c755
Zachary Tong <zach@zacharytong.com>
2017-08-21 17:40:36 -0400

    Shrink API was not setting body correctly

--- src/Elasticsearch/Namespaces/IndicesNamespace.php
+++ src/Elasticsearch/Namespaces/IndicesNamespace.php
@@ -248,6 +248,7 @@ class IndicesNamespace extends AbstractNamespace
     {
         $index = $this->extractArgument($params, 'index');
         $target = $this->extractArgument($params, 'target');
+        $body = $this->extractArgument($params, 'body');
 
         /** @var callback $endpointBuilder */
         $endpointBuilder = $this->endpoints;
@@ -255,7 +256,8 @@ class IndicesNamespace extends AbstractNamespace
         /** @var \Elasticsearch\Endpoints\Indices\Shrink $endpoint */
         $endpoint = $endpointBuilder('Indices\Shrink');
         $endpoint->setIndex($index)
-                 ->setTarget($target);
+                 ->setTarget($target)
+                 ->setBody($body);
 
         return $this->performRequest($endpoint);
     }

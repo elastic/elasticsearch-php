b41c7330de3273f1947cac6ee65702eca6cf4c1b
Zachary Tong <zach@zacharytong.com>
2017-08-21 17:40:18 -0400

    [TEST] Add some missing and required static configs

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -21,6 +21,12 @@ unzip "elasticsearch-latest-SNAPSHOT.zip"
 echo "Adding repo to config..."
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'repositories.url.allowed_urls: ["http://*"]' >> $TXT ; done
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'path.repo: ["/tmp"]' >> $TXT ; done
+find . -name "elasticsearch.yml" | while read TXT ; do echo 'node.max_local_storage_nodes: 1' >> $TXT ; done
+find . -name "elasticsearch.yml" | while read TXT ; do echo 'cluster.routing.allocation.disk.watermark.low: 1b' >> $TXT ; done
+find . -name "elasticsearch.yml" | while read TXT ; do echo 'cluster.routing.allocation.disk.watermark.high: 1b' >> $TXT ; done
+find . -name "elasticsearch.yml" | while read TXT ; do echo 'node.attr.testattr: test' >> $TXT ; done
+find . -name "elasticsearch.yml" | while read TXT ; do echo 'script.max_compilations_per_minute: 2048' >> $TXT ; done
+
 
 echo "Starting Elasticsearch v${ES_VERSION}"
 

9b719402bc30c49e56e341c1408fc730171a68b0
Zachary Tong <zach@zacharytong.com>
2017-08-21 18:45:34 -0400

    [TEST] remove watermark flood from static config
    Not compatible with 5.x, and I don't think we need it anyway.
    If needed later we can add some sort of check, although
    version comparisons in bash are gross (and we have to deal with
    .x versions)

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -4,7 +4,6 @@ if [ -z $ES_VERSION ]; then
     exit 1;
 fi;
 
-
 killall java 2>/dev/null
 
 which java
@@ -24,10 +23,8 @@ find . -name "elasticsearch.yml" | while read TXT ; do echo 'path.repo: ["/tmp"]
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'node.max_local_storage_nodes: 1' >> $TXT ; done
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'cluster.routing.allocation.disk.watermark.low: 1b' >> $TXT ; done
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'cluster.routing.allocation.disk.watermark.high: 1b' >> $TXT ; done
-find . -name "elasticsearch.yml" | while read TXT ; do echo 'cluster.routing.allocation.disk.watermark.flood_stage: 1b' >> $TXT ; done
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'node.attr.testattr: test' >> $TXT ; done
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'script.max_compilations_per_minute: 2048' >> $TXT ; done
-
 
 echo "Starting Elasticsearch v${ES_VERSION}"
 

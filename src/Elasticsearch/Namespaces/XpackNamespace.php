3eece756d62c3acb812dd44d6befeb34085761fc
Augustin Husson <husson.augustin@gmail.com>
2017-12-03 01:10:13 +0100

    use Xpack/Get endpoint and performRequest

--- src/Elasticsearch/Namespaces/XpackNamespace.php
+++ src/Elasticsearch/Namespaces/XpackNamespace.php
@@ -6,4 +6,20 @@ namespace Elasticsearch\Namespaces;
 class XpackNamespace extends AbstractNamespace
 {
 
+    /**
+     * @param array $params
+     * @return array
+     */
+    public function get($params = array())
+    {
+        /** @var callback $endpointBuilder */
+        $endpointBuilder = $this->endpoints;
+
+        /** @var \Elasticsearch\Endpoints\Xpack\Get $endpoint */
+        $endpoint = $endpointBuilder('Xpack\Get');
+
+        $endpoint->setParams($params);
+
+        return $this->performRequest($endpoint);
+    }
 }

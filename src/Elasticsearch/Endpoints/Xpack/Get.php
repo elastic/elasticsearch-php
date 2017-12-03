6e76aed44be193b8da077811e618d00c57630229
Augustin Husson <husson.augustin@gmail.com>
2017-12-03 12:26:34 +0100

    add phpdoc

--- src/Elasticsearch/Endpoints/Xpack/Get.php
+++ src/Elasticsearch/Endpoints/Xpack/Get.php
@@ -2,9 +2,17 @@
 
 namespace Elasticsearch\Endpoints\Xpack;
 
-
 use Elasticsearch\Endpoints\AbstractEndpoint;
 
+/**
+ * Class Get
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Endpoints\Xpack
+ * @author   Augustin Husson <husson.augustin@gmail.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elastic.co
+ */
 class Get extends AbstractEndpoint
 {
 
--- src/Elasticsearch/Namespaces/XpackNamespace.php
+++ src/Elasticsearch/Namespaces/XpackNamespace.php
@@ -2,12 +2,22 @@
 
 namespace Elasticsearch\Namespaces;
 
-
+/**
+ * Class XpackNamespace
+ *
+ * @category Elasticsearch
+ * @package  Elasticsearch\Namespaces
+ * @author   Augustin Husson <husson.augustin@gmail.com>
+ * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
+ * @link     http://elastic.co
+ */
 class XpackNamespace extends AbstractNamespace
 {
 
     /**
-     * @param array $params
+     * $param['categories']    = (list) A comma-separated list of the information categories to include in the response. For example, build,license,features
+     *       ['humans']        = (boolean) Defines whether additional human-readable information is included in the response. In particular, it adds descriptions and a tag line. The default value is true.
+     * @param array $params Associative array of parameters
      * @return array
      */
     public function get($params = array())

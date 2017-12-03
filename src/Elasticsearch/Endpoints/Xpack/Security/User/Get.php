8b27cf6a55f8a2dce377b3ac90ff71ea2180df1c
Augustin Husson <husson.augustin@gmail.com>
2017-12-03 19:30:32 +0100

    use AbstractUserEndpoint instead of directly AbstractEndpoint

--- src/Elasticsearch/Endpoints/Xpack/Security/User/Get.php
+++ src/Elasticsearch/Endpoints/Xpack/Security/User/Get.php
@@ -2,21 +2,17 @@
 
 namespace Elasticsearch\Endpoints\Xpack\Security\User;
 
-use Elasticsearch\Endpoints\AbstractEndpoint;
-
 /**
  * Class Get
  *
  * @category Elasticsearch
- * @package Elasticsearch\Endpoints\Xpack\Security\User
+ * @package  Elasticsearch\Endpoints\Xpack\Security\User
  * @author   Augustin Husson <husson.augustin@gmail.com>
  * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
  * @link     http://elastic.co
  */
-class Get extends AbstractEndpoint
+class Get extends AbstractUserEndpoint
 {
-
-    protected $username;
 
     /**
      * @return string[]
@@ -46,21 +42,5 @@ class Get extends AbstractEndpoint
     public function getMethod()
     {
         return 'GET';
-    }
-
-
-    /**
-     * @param $username string
-     * @return Get
-     */
-    public function setUsername($username)
-    {
-        if (isset($username) !== true) {
-            return $this;
-        }
-
-        $this->username = $username;
-
-        return $this;
     }
 }

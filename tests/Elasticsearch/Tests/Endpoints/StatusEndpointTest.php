f1c5c1c07f3d90fa85a5075bab46b181893e40fb
Adam Plante <toomanyadams@gmail.com>
2018-01-05 16:08:55 -0800

    Fix issue with getting status of respository and snapshots.

---
+++ tests/Elasticsearch/Tests/Endpoints/StatusEndpointTest.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Elasticsearch\Tests\Endpoints;
+
+use Elasticsearch\Endpoints\Snapshot\Status;
+use Elasticsearch\Common\Exceptions;
+
+class StatusEndpointTest extends \PHPUnit\Framework\TestCase
+{
+    private $endpoint;
+
+    protected function setUp()
+    {
+        $this->endpoint = new Status();
+    }
+
+    public static function statusParams()
+    {
+        return [
+            [
+                'repository' => 'my_backup',
+                'snapshot' => null,
+                'expected' => '/_snapshot/my_backup/_status',
+            ],
+            [
+                'repository' => 'my_backup',
+                'snapshot' => 'snapshot_1',
+                'expected' => '/_snapshot/my_backup/snapshot_1/_status',
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider statusParams
+     */
+    public function testGetUriReturnsAppropriateUri($repository, $snapshot, $expected)
+    {
+        if ($repository) {
+            $this->endpoint->setRepository($repository);
+        }
+
+        if ($snapshot) {
+            $this->endpoint->setSnapshot($snapshot);
+        }
+
+        $this->assertSame($expected, $this->endpoint->getURI());
+    }
+
+    public function testMissingRepositoryThrowsException()
+    {
+
+        $this->expectException(Exceptions\RuntimeException::class);
+
+        $this->endpoint->setSnapshot('should fail');
+        $this->endpoint->getURI();
+    }
+}

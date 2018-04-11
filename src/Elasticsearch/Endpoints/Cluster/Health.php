98a372c90da8a6f415104fc274515e7b7f528ba9
Zachary Tong <zach@zacharytong.com>
2018-04-11 13:56:54 +0000

    Add 'wait_for_no_initializing_shards' to Cluster\Health whitelist

--- src/Elasticsearch/Endpoints/Cluster/Health.php
+++ src/Elasticsearch/Endpoints/Cluster/Health.php
@@ -45,7 +45,8 @@ class Health extends AbstractEndpoint
             'wait_for_relocating_shards',
             'wait_for_status',
             'wait_for_events',
-            'wait_for_no_relocating_shards'
+            'wait_for_no_relocating_shards',
+            'wait_for_no_initializing_shards',
         );
     }
 

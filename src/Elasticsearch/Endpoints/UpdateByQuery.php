2105a7ca4e197621a36a8fbb43ff194d37adafc6
Minh Kun <tquocminh17@gmail.com>
2018-09-04 12:21:39 +0800

    Adding slices into white list

--- src/Elasticsearch/Endpoints/UpdateByQuery.php
+++ src/Elasticsearch/Endpoints/UpdateByQuery.php
@@ -107,6 +107,7 @@ class UpdateByQuery extends AbstractEndpoint
             'consistency',
             'scroll_size',
             'wait_for_completion',
+            'slices',
         ];
     }
 

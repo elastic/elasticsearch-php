f3a2b0f8fcf3d77e135a1985a2e833fce51e0cd6
Zachary Tong <zach@elastic.co>
2019-01-08 13:02:12 -0500

    [TEST] Download artifacts directly, migrate off esvm
    esvm is no longer maintained (or only sporadically), so move to the
    direct artifacts.  Not a good long-term solution since we can't (easily)
    get the most recent patch release in each branch, but is better than
    being stuck at 6.3 with esvm.

--- travis/download_and_run_es.sh
+++ travis/download_and_run_es.sh
@@ -10,12 +10,12 @@ which java
 java -version
 
 
-echo "Downloading Elasticsearch v${ES_VERSION}-SNAPSHOT..."
+echo "Downloading Elasticsearch v${ES_VERSION}..."
 
-ES_URL=$(curl -sS "https://esvm-props.kibana.rocks/builds" | jq -r ".branches[\"$ES_VERSION\"].zip")
+ES_URL="https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-${ES_VERSION}.tar.gz"
 
-curl -L -o elasticsearch-latest-SNAPSHOT.zip $ES_URL
-unzip "elasticsearch-latest-SNAPSHOT.zip"
+curl -L -o elasticsearch-latest.tar.gz $ES_URL
+tar -xvfz "elasticsearch-latest.tar.gz"
 
 echo "Adding repo to config..."
 find . -name "elasticsearch.yml" | while read TXT ; do echo 'repositories.url.allowed_urls: ["http://*"]' >> $TXT ; done

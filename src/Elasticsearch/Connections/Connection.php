29d81a5180ec1711acef979c796affe042486334
Enrico Zimuel <e.zimuel@gmail.com>
2020-12-18 22:54:46 +0100

    Fixed x-elastic-client-meta header with lowercase value

--- src/Elasticsearch/Connections/Connection.php
+++ src/Elasticsearch/Connections/Connection.php
@@ -583,21 +583,26 @@ class Connection implements ConnectionInterface
 
     /**
      * Get the x-elastic-client-meta header
+     * 
+     * The header format is specified by the following regex:
+     * ^[a-z]{1,}=[a-z0-9\.\-]{1,}(?:,[a-z]{1,}=[a-z0-9\.\-]+)*$
      */
     private function getElasticMetaHeader(array $connectionParams): string
     {
         $phpSemVersion = sprintf("%d.%d.%d", PHP_MAJOR_VERSION, PHP_MINOR_VERSION, PHP_RELEASE_VERSION);
+        // Reduce the size in case of '-snapshot' version
+        $clientVersion = str_replace('-snapshot', '-s', strtolower(Client::VERSION)); 
         $clientMeta = sprintf(
             "es=%s,php=%s,t=%s,a=%d",
-            Client::VERSION,
+            $clientVersion,
             $phpSemVersion,
-            Client::VERSION,
+            $clientVersion,
             isset($connectionParams['client']['future']) && $connectionParams['client']['future'] === 'lazy' ? 1 : 0
         );
         if (function_exists('curl_version')) {
             $curlVersion = curl_version();
             if (isset($curlVersion['version'])) {
-                $clientMeta .= sprintf(",c=%s", $curlVersion['version']);
+                $clientMeta .= sprintf(",cu=%s", $curlVersion['version']); // cu = curl library
             }
         }
         return $clientMeta;

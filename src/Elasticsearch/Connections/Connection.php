78613c421dc487e90bf617c31d3088e5fb14ac79
Jiří Slischka <slischkaj@gmail.com>
2020-12-19 08:32:36 +0100

    Removed unused variable

--- src/Elasticsearch/Connections/Connection.php
+++ src/Elasticsearch/Connections/Connection.php
@@ -342,7 +342,7 @@ class Connection implements ConnectionInterface
         if (isset($params) === true && !empty($params)) {
             array_walk(
                 $params,
-                function (&$value, &$key) {
+                function (&$value) {
                     if ($value === true) {
                         $value = 'true';
                     } elseif ($value === false) {

18d0fde8eb7e726230684e871f25530802a04243
Reinder Reinders <reinder@level23.nl>
2021-01-11 13:36:39 +0100

    Check if current offset exists in connections array

--- tests/Elasticsearch/Tests/ConnectionPool/Selectors/StickyRoundRobinSelectorTest.php
+++ tests/Elasticsearch/Tests/ConnectionPool/Selectors/StickyRoundRobinSelectorTest.php
@@ -29,7 +29,7 @@ use Mockery as m;
  */
 class StickyRoundRobinSelectorTest extends \PHPUnit\Framework\TestCase
 {
-    public function tearDown(): void
+    public function tearDown()
     {
         m::close();
     }
@@ -73,5 +73,40 @@ class StickyRoundRobinSelectorTest extends \PHPUnit\Framework\TestCase
 
             $this->assertSame($mockConnections[1], $retConnection);
         }
+    }
+
+    public function testConnectionDroppedBySniffingPool()
+    {
+        $roundRobin = new Elasticsearch\ConnectionPool\Selectors\StickyRoundRobinSelector();
+
+        $mockConnections = [];
+        foreach (range(0, 3) as $index) {
+            $mockConnections[] = m::mock(ConnectionInterface::class)
+                ->shouldReceive('isAlive')->once()->andReturn(false)->getMock();
+        }
+
+        $mockConnections[] = m::mock(ConnectionInterface::class)
+            ->shouldReceive('isAlive')->once()->andReturn(true)->getMock();
+
+        // check we're getting the next one each time
+        foreach (range(0, 3) as $index) {
+            $retConnection = $roundRobin->select($mockConnections);
+            $this->assertSame($mockConnections[$index + 1], $retConnection);
+        }
+
+        // current is 4 now inside the StickyRoundRobinSelector();
+        $retConnection = $roundRobin->select($mockConnections);
+        $this->assertSame($mockConnections[4], $retConnection);
+
+        // now we are going to simulate 1 server dropped out, and thus only 4 connections are giving back, which
+        // resulted in Undefined offset: 4 in Elasticsearch/ConnectionPool/Selectors/StickyRoundRobinSelector.php:45
+        $mockConnections = [];
+        foreach (range(0, 3) as $index) {
+            $mockConnections[] = m::mock(ConnectionInterface::class);
+        }
+
+        // instead of index 4 we should retrieve index 1 (5 % 4 = 1)
+        $retConnection = $roundRobin->select($mockConnections);
+        $this->assertSame($mockConnections[1], $retConnection);
     }
 }

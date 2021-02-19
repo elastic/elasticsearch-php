02d45907605989add6f013ff9fb3986bb15001b2
Enrico Zimuel <e.zimuel@gmail.com>
2021-02-19 11:20:10 +0100

    Updated phpunit, CI, github configurations

--- .ci/yaml-tests.sh
+++ .ci/yaml-tests.sh
@@ -3,5 +3,8 @@
 # Checkout the YAML test from Elasticsearch tag
 php util/RestSpecRunner.php
 
+# Generate the YAML tests for PHPUnit
+php util/build_tests.php ${STACK_VERSION} ${TEST_SUITE}
+
 # Run YAML tests
-vendor/bin/phpunit -c phpunit-yaml-tests.xml
+vendor/bin/phpunit -c "phpunit-yaml-${TEST_SUITE}-tests.xml"

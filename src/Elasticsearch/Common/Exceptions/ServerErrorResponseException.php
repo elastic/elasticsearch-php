d20a5bc4d32447fc4444555a318ecbd353ab5459
Enrico Zimuel <e.zimuel@gmail.com>
2021-02-22 12:36:24 +0100

    Added the license header

--- src/Elasticsearch/Common/Exceptions/ServerErrorResponseException.php
+++ src/Elasticsearch/Common/Exceptions/ServerErrorResponseException.php
@@ -1,4 +1,18 @@
 <?php
+/**
+ * Elasticsearch PHP client
+ *
+ * @link      https://github.com/elastic/elasticsearch-php/
+ * @copyright Copyright (c) Elasticsearch B.V (https://www.elastic.co)
+ * @license   http://www.apache.org/licenses/LICENSE-2.0 Apache License, Version 2.0
+ * @license   https://www.gnu.org/licenses/lgpl-2.1.html GNU Lesser General Public License, Version 2.1 
+ * 
+ * Licensed to Elasticsearch B.V under one or more agreements.
+ * Elasticsearch B.V licenses this file to you under the Apache 2.0 License or
+ * the GNU Lesser General Public License, Version 2.1, at your option.
+ * See the LICENSE file in the project root for more information.
+ */
+
 
 declare(strict_types = 1);
 
@@ -7,11 +21,6 @@ namespace Elasticsearch\Common\Exceptions;
 /**
  * ServerErrorResponseException
  *
- * @category Elasticsearch
- * @package  Elasticsearch\Common\Exceptions
- * @author   Zachary Tong <zach@elastic.co>
- * @license  http://www.apache.org/licenses/LICENSE-2.0 Apache2
- * @link     http://elastic.co
  */
 class ServerErrorResponseException extends TransportException implements ElasticsearchException
 {

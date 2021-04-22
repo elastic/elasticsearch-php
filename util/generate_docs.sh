40d0d86fdf609be733751f1688540e0b167ea7f7
William Desportes <williamdes@wdes.fr>
2021-04-22 02:21:40 +0200

    Upgrade Doctum to 5.4.1 and twig templates to Twig 3

--- util/generate_docs.sh
+++ util/generate_docs.sh
@@ -6,17 +6,18 @@ set -e
 rm -f doctum.phar
 rm -f doctum.phar.sha256
 
-# Download the latest (5.1.x) release
-curl -O https://doctum.long-term.support/releases/5.1/doctum.phar
-curl -O https://doctum.long-term.support/releases/5.1/doctum.phar.sha256
+# Download the latest (5.x.x) release
+curl -O https://doctum.long-term.support/releases/5/doctum.phar
+curl -O https://doctum.long-term.support/releases/5/doctum.phar.sha256
 
 sha256sum --strict --check doctum.phar.sha256
 rm -f doctum.phar.sha256
-# You can fetch the latest (5.1.x) version code here:
-# https://doctum.long-term.support/releases/5.1/VERSION
+chmod +x ./doctum.phar
+# You can fetch the latest (5.x.x) version code here:
+# https://doctum.long-term.support/releases/5/VERSION
 
 # Show the version to inform users of the script
-php doctum.phar --version
+./doctum.phar version --text
 
 # Only exit if the process fails, not if parse errors are found
-php doctum.phar update --force --ignore-parse-errors -v util/docsConfig.php
+./doctum.phar update --force --ignore-parse-errors -v util/docsConfig.php

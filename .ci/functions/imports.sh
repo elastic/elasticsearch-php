f95262449bc8708ea73affe20ebd4270190055fe
Enrico Zimuel <e.zimuel@gmail.com>
2021-04-23 09:07:16 +0200

    Running elasticsearch only with platinum settings

--- .ci/functions/imports.sh
+++ .ci/functions/imports.sh
@@ -27,9 +27,6 @@ if [[ -z $es_node_name ]]; then
   export elastic_password=changeme
   export elasticsearch_image=elasticsearch
   export elasticsearch_url=https://elastic:${elastic_password}@${es_node_name}:9200
-  if [[ $TEST_SUITE != "platinum" ]]; then
-    export elasticsearch_url=http://${es_node_name}:9200
-  fi
   export external_elasticsearch_url=${elasticsearch_url/$es_node_name/localhost}
   export elasticsearch_container="${elasticsearch_image}:${STACK_VERSION}"
 

afb08129a0e2c140ed75bfbd8a697d2c234272cd
Philipp Wahala <philipp.wahala@gmail.com>
2022-09-10 08:39:28 +0200

    Add return types to MessageResponseTrait to match upcoming PSR interface

--- src/Response/Elasticsearch.php
+++ src/Response/Elasticsearch.php
@@ -198,7 +198,7 @@ class Elasticsearch implements ElasticsearchInterface, ResponseInterface, ArrayA
      * @see https://www.php.net/manual/en/class.arrayaccess.php
      */
     #[\ReturnTypeWillChange]
-    public function offsetGet($offset)
+    public function offsetGet($offset): mixed
     {
         return $this->asArray()[$offset];
     }
--- src/Traits/MessageResponseTrait.php
+++ src/Traits/MessageResponseTrait.php
@@ -22,72 +22,72 @@ use Psr\Http\Message\StreamInterface;
  */
 trait MessageResponseTrait
 {
-    public function getProtocolVersion()
+    public function getProtocolVersion(): string
     {
         return $this->response->getProtocolVersion();
     }
 
-    public function withProtocolVersion($version)
+    public function withProtocolVersion($version): static
     {
         return $this->response->withProtocolVersion($version);
     }
 
-    public function getHeaders()
+    public function getHeaders(): array
     {
         return $this->response->getHeaders();
     }
 
-    public function hasHeader($name)
+    public function hasHeader($name): bool
     {
         return $this->response->hasHeader($name);
     }
 
-    public function getHeader($name)
+    public function getHeader($name): array
     {
         return $this->response->getHeader($name);
     }
 
-    public function getHeaderLine($name)
+    public function getHeaderLine($name): string
     {
         return $this->response->getHeaderLine($name);
     }
 
-    public function withHeader($name, $value)
+    public function withHeader($name, $value): static
     {
         return $this->response->withHeader($name, $value);
     }
 
-    public function withAddedHeader($name, $value)
+    public function withAddedHeader($name, $value): static
     {
         return $this->response->withAddedHeader($name, $value);
     }
 
-    public function withoutHeader($name)
+    public function withoutHeader($name): static
     {
         return $this->response->withoutHeader($name);
     }
 
-    public function getBody()
+    public function getBody(): StreamInterface
     {
         return $this->response->getBody();
     }
 
-    public function withBody(StreamInterface $body)
+    public function withBody(StreamInterface $body): static
     {
         return $this->response->withBody($body);
     }
 
-    public function getStatusCode()
+    public function getStatusCode(): int
     {
         return $this->response->getStatusCode();
     }
 
-    public function withStatus($code, $reasonPhrase = '')
+    public function withStatus($code, $reasonPhrase = ''): static
     {
         return $this->response->withStatus($code, $reasonPhrase);
     }
 
-    public function getReasonPhrase()
+    public function getReasonPhrase(): string
     {
         return $this->response->getReasonPhrase();
     }

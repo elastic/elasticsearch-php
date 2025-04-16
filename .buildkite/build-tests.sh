5c5ec8473a99566b57a753f5629b63097fc3b8e3
Enrico Zimuel <e.zimuel@gmail.com>
2025-04-16 16:03:23 +0200

    Added export functions in build-tests

--- .buildkite/build-tests.sh
+++ .buildkite/build-tests.sh
@@ -9,16 +9,23 @@ script_path=$(dirname $(realpath -s $0))
 set -euo pipefail
 
 PHP_VERSION=${PHP_VERSION-8.4-cli}
-ELASTICSEARCH_URL=${ELASTICSEARCH_URL-"$elasticsearch_url"}
 elasticsearch_container=${elasticsearch_container-}
 
 echo -e "\033[34;1mINFO:\033[0m VERSION ${STACK_VERSION}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m TEST_GROUP ${TEST_GROUP}\033[0m"
-echo -e "\033[34;1mINFO:\033[0m URL ${ELASTICSEARCH_URL}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m CONTAINER ${elasticsearch_container}\033[0m"
 echo -e "\033[34;1mINFO:\033[0m PHP_VERSION ${PHP_VERSION}\033[0m"
 
 echo -e "\033[1m>>>>> Build docker container >>>>>>>>>>>>>>>>>>>>>>>>>>>>>\033[0m"
+
+export elasticsearch_image=elasticsearch
+export elasticsearch_container="${elasticsearch_image}:${STACK_VERSION}"
+export suffix=rest-test
+export moniker=$(echo "$elasticsearch_container" | tr -C "[:alnum:]" '-')
+export network_name=${moniker}${suffix}
+
+echo -e "\033[34;1mINFO:\033[0m Creating network $network_name if it does not exist already \033[0m"
+docker network inspect "$network_name" > /dev/null 2>&1 || docker network create "$network_name"
 
 docker build \
   --no-cache \
@@ -38,7 +45,6 @@ docker run \
   --env STACK_VERSION=${STACK_VERSION} \
   --env TEST_GROUP=${TEST_GROUP} \
   --env PHP_VERSION=${PHP_VERSION} \
-  --env ELASTICSEARCH_URL=${ELASTICSEARCH_URL} \
   --ulimit nofile=65535:65535 \
   --name elasticsearch-php \
   --rm \

89f34dde0932d74d3dbc45165ba5717356d69f87
Enrico Zimuel <e.zimuel@gmail.com>
2025-04-18 10:22:45 +0200

    Added header license to test_to_skip.php

--- tests/test_to_skip.php
+++ tests/test_to_skip.php
@@ -1,4 +1,16 @@
 <?php
+/**
+ * Elasticsearch PHP Client
+ *
+ * @link      https://github.com/elastic/elasticsearch-php
+ * @copyright Copyright (c) Elasticsearch B.V (https://www.elastic.co)
+ * @license   https://opensource.org/licenses/MIT MIT License
+ *
+ * Licensed to Elasticsearch B.V under one or more agreements.
+ * Elasticsearch B.V licenses this file to you under the MIT License.
+ * See the LICENSE file in the project root for more information.
+ */
+
 // A list of tests in elasticsearch-clients-tests to be skipped
 return [
     'cat/aliases.yml',

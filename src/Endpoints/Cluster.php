7bca9f41d297b51d536b467d9857af35ad8971cf
Elastic Machine <elasticmachine@users.noreply.github.com>
2026-02-09 18:01:56 +0100

    Auto-generated API code (#1523)

--- src/Endpoints/Cluster.php
+++ src/Endpoints/Cluster.php
@@ -488,7 +488,7 @@ class Cluster extends AbstractEndpoint
 	 * @link https://www.elastic.co/guide/en/elasticsearch/reference/8.19/cluster-update-settings.html
 	 *
 	 * @param array{
-	 *     flat_settings?: bool, // Return settings in flat format (default: false)
+	 *     flat_settings?: bool, // Return settings in flat format
 	 *     master_timeout?: int|string, // Explicit operation timeout for connection to master node
 	 *     timeout?: int|string, // Explicit operation timeout
 	 *     pretty?: bool, // Pretty format the returned JSON response. (DEFAULT: false)

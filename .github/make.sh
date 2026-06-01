1792c6eff341794962b16f8f0cbe453734315036
Fernando Briano <fernando@picandocodigo.net>
2026-06-01 08:31:30 +0100

    [CI] Updates bumpmatrix task in make.sh

--- .github/make.sh
+++ .github/make.sh
@@ -168,7 +168,7 @@ if [[ "$CMD" == "bump" ]]; then
 fi
 
 if [[ "$CMD" == "bumpmatrix" ]]; then
-  TEST_FILES=".github/workflows/test.yml .github/workflows/integration_test.yml .github/workflows/yaml_test.yml"
+  TEST_FILES=".github/workflows/test.yml .github/workflows/integration_test.yml .buildkite/pipeline.yml"
   for TEST_FILE in $TEST_FILES; do
       sed -E -i.bak 's/[0-9]+\.[0-9]+\.[0-9]+-SNAPSHOT/'$VERSION'/g' ${TEST_FILE}
       rm ${TEST_FILE}.bak
